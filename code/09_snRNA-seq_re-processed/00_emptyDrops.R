987584cc93d0046314ebda98b7d4c6c06ceeaacd
joshstolz <josh.stolz@libd.org>
2022-03-10 11:52:08 -0500

    make gtf annotation functional by add gtf_cols

--- code/09_snRNA-seq_re-processed/00_emptyDrops.R
+++ code/09_snRNA-seq_re-processed/00_emptyDrops.R
@@ -75,7 +75,7 @@ e.out.hb.1092 <- emptyDrops(counts(s3e.hb.1092), niters=20000)
 e.out.hb.5555 <- emptyDrops(counts(s3e.hb.5555), niters=20000)
 e.out.hb.5558 <- emptyDrops(counts(s3e.hb.5558), niters=20000)
 ####################################
-########subset for empty Drops #####
+########Subset for Empty Drops #####
 ####################################
 
 s3e.hb.1204 <- s3e.hb.1204[ ,which(e.out.hb.1204$FDR <= 0.001)]
@@ -90,11 +90,47 @@ sce.all.hb <- cbind(s3e.hb.1204, s3e.hb.5558, s3e.hb.1469, s3e.hb.5639, s3e.hb.1
 rm(s3e.hb.1469,s3e.hb.1204,s3e.hb.5558,s3e.hb.5639, s3e.hb.1735, s3e.hb.1092,s3e.hb.5555)
 
 
-dim(sce.all.hb )
+dim(sce.all.hb)
 # [1] 36601 19864
 
 #(barcode whitelist must be ~6.8 million potential barcodes?)
-rownames(sce.all.hb ) <- uniquifyFeatureNames(rowData(sce.all.hb )$ID, rowData(sce.all.hb )$Symbol)
+rownames(sce.all.hb) <- uniquifyFeatureNames(rowData(sce.all.hb)$Symbol, rowData(sce.all.hb)$ID)
 
-save(sce.all.hb, e.out.hb.1204, e.out.hb.1469, e.out.hb.5639, e.out.hb.1735, e.out.hb.1092, e.out.hb.5555,
+
+gtf_cols = c("source", "type", "gene_id", "gene_version", "gene_name", "gene_type")
+gtf <- rtracklayer::import("/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A/genes/genes.gtf")
+    gtf <- gtf[gtf$type == "gene"]
+    names(gtf) <- gtf$gene_id
+
+
+    ## Match the genes
+    if (verbose) message(Sys.time(), " adding gene information to the sce.all.hb object")
+    match_genes <- match(rownames(sce.all.hb), gtf$gene_id)
+
+    if (all(is.na(match_genes))) {
+        ## Protect against scenario where one set has GENCODE IDs and the other one has ENSEMBL IDs.
+        warning("Gene IDs did not match. This typically happens when you are not using the same GTF file as the one that was used by SpaceRanger. For example, one file uses GENCODE IDs and the other one ENSEMBL IDs. read10xVisiumWrapper() will try to convert them to ENSEMBL IDs.", call. = FALSE)
+        match_genes <- match(gsub("\\..*", "", rownames(sce.all.hb)), gsub("\\..*", "", gtf$gene_id))
+    }
+
+    if (any(is.na(match_genes))) {
+        warning("Dropping ", sum(is.na(match_genes)), " out of ", length(match_genes), " genes for which we don't have information on the reference GTF file. This typically happens when you are not using the same GTF file as the one that was used by SpaceRanger.", call. = FALSE)
+        ## Drop the few genes for which we don't have information
+        sce.all.hb <- sce.all.hb[!is.na(match_genes), ]
+        match_genes <- match_genes[!is.na(match_genes)]
+    }
+
+    ## Keep only some columns from the gtf
+    mcols(gtf) <- mcols(gtf)[, gtf_cols[gtf_cols %in% colnames(mcols(gtf))]]
+
+    ## Add the gene info to our sce.all.hb object
+    rowRanges(sce.all.hb) <- gtf[match_genes]
+
+
+
+save(sce.all.hb,
      file=here("processed-data","08_snRNA-seq_Erik","20220301_human_hb_processing.rda"))
+
+save(e.out.hb.1204, e.out.hb.1469, e.out.hb.5639, e.out.hb.1735, e.out.hb.1092, e.out.hb.5555,
+     file=here("processed-data","08_snRNA-seq_Erik","20220301_human_hb_processing_drops.rda"))
+
