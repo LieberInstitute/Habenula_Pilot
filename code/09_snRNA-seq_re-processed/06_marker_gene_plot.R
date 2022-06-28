a99da596744df841b99d595fce29a177948182a0
joshstolz <josh.stolz@libd.org>
2022-06-28 16:07:45 -0400

    add plots for regions and hab markers

--- code/09_snRNA-seq_re-processed/06_marker_gene_plot.R
+++ code/09_snRNA-seq_re-processed/06_marker_gene_plot.R
@@ -17,6 +17,55 @@ load(here("processed-data","09_snRNA-seq_re-processed","matt_markers","markers.r
 load(here("processed-data","09_snRNA-seq_re-processed","05_collapsedClustering.Rda"))
 
 
+Astro = list(
+    "ETNPPL",
+    "LINC00499",
+    "MRVI1",
+    "GJA1",
+    "SDC4")
+
+Excit = list(
+    "LINC02217",
+    "MLIP",
+    "AC067956.1",
+    "AC019211.1",
+    "NEUROD2"
+)
+
+OPC = list(
+    "CSPG4",
+    "GALR1",
+    "GPR17",
+    "AC004852.2",
+    "PDGFRA"
+)
+
+
+Oligo = list(
+    "TMEM235",
+    "MAG",
+    "SH3TC2",
+    "LINC01608",
+    "CD22"
+)
+
+Micro = list(
+    "FGD2",
+    "CD86",
+    "CX3CR1",
+    "LINC00996",
+    "SYK"
+)
+
+Inhib = list(
+    "DLX6-AS1",
+    "GAD2",
+    "GRIP2",
+    "ANK1",
+    "SLC35F4"
+)
+
+markers.eric.hab = list(Inhib = Inhib,Excit = Excit,Astro = Astro,Micro = Micro,Oligo = Oligo,OPC = OPC)
 plotExpressionCustom <- function(sce, features, features_name, anno_name = "cellType",
                                  point_alpha=0.2, point_size=0.7, ncol=2, xlab = NULL,
                                  exprs_values = "logcounts", scales = "fixed"){
@@ -54,7 +103,32 @@ for(i in 1:length(markers.mathys.custom)){
 }
 dev.off()
 
+rownames(sce.all.hb)<-rowData(sce.all.hb)$gene_name
+pdf(here("plots","09_snRNA-seq_re-processed", "regionSpecific_Hab-n7_marker-logExprs_habMarkers_collapsedClusters_JMS2022.pdf"), height=6, width=8)
+for(i in 1:length(markers.eric.hab)){
+  print(
+    plotExpressionCustom(sce = sce.all.hb,
+                         features = markers.eric.hab[[i]],
+                         features_name = names(markers.eric.hab)[[i]],
+                         anno_name = "collapsedCluster")
+  )
+}
+dev.off()
 
+
+region.markers<- list(habenula = c("POU2F2","POU4F1","GPR151"),medial_habenula = c("CHRNB3","CHAT","TAC1"),lateral_habenula = c("HTR2C","HTR4"),broad_thalamus = c("LYPD6B","ADARB2","RORB"),specific_thalamus = c("INHBA","NPTXR"))
+
+
+pdf(here("plots","09_snRNA-seq_re-processed", "regionSpecific_Hab-n7_marker-logExprs_regionMarkers_prelimClusters_JMS2022.pdf"), height=6, width=8)
+for(i in 1:length(region.markers)){
+  print(
+    plotExpressionCustom(sce = sce.all.hb,
+                         features = region.markers[[i]],
+                         features_name = names(region.markers)[[i]],
+                         anno_name = "prelimCluster")
+  )
+}
+dev.off()
 
 print("Reproducibility information:")
 Sys.time()
