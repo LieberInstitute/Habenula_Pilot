a53c67e6fb40a722e4018b5be3bf19e200d28fbe
ajanakub <bukolaajanaku@gmail.com>
2022-09-29 12:51:33 -0400

    Finally able to calculate logcounts but now reconsidering rse object in terms of plotting. Will need to subset by sex and age. Need to furhter adapt Josh's code

--- code/70_GPR151/rseGenePlot.R
+++ code/70_GPR151/rseGenePlot.R
@@ -1,12 +1,14 @@
 # September 27th, 2022
 # RSE Gene Violin Plots - Bukola Ajanaku
-# R
+# qrsh -l mem_free=24G,h_vmem=100G
 
 library("SummarizedExperiment")
 library("GenomicRanges")
 library("ggplot2")
 library("edgeR")
-library("jaffelab")
+library ("sessioninfo") # trying to incorporate this package
+
+
 
 # Loads merged rse_gene from Geo, includes data from Leo.
 load("/dcs04/lieber/lcolladotor/dbDev_LIBD001/RNAseq/4Bukola/rse_gene.merged.curated.n5780.rda")
@@ -22,9 +24,8 @@ filtRSE <- rse[, rse$Protocol == "RiboZeroGold"]
 assays(filtRSE, withDimnames = FALSE)$logcounts <-
     edgeR::cpm(calcNormFactors(filtRSE, method = "TMM"), log = TRUE, prior.count = 0.5)
 
-
 # Subsets rse for GPR151.
 subGPR <- filtRSE[rowData(filtRSE)$Symbol == "GPR151", ]
 
-
 # Makes violin plots for logcounts of GPR151 by brain region.
+
