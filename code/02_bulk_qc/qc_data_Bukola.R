2a3c0348d5033e9e9c1281509b4646f55a28f385
ajanakub <bukolaajanaku@gmail.com>
2022-10-31 10:58:01 -0400

    Continued with gene data for quality control metrics in checking for brain swaps between RNA and genomic data. Should correlate but we may need to swap samples.

--- code/02_bulk_qc/qc_data_Bukola.R
+++ code/02_bulk_qc/qc_data_Bukola.R
@@ -20,7 +20,7 @@ load(here("preprocessed_data", "rse_tx_Roche_Habenula_PairedEnd_n73.Rdata")) #tr
 dir.create(here("preprocessed_data","qc_qlots_bukola"))
 dir.create(here("preprocessed_data","count_data_bukola"))
 
-# original flow cell [ask Louise about]
+# original flow cell
 pd = colData(rse_gene)
 man = read.delim(here("preprocessed_data", ".samples_unmerged.manifest"), 
                  as.is=TRUE,header=FALSE)
@@ -103,10 +103,131 @@ pdf("preprocessed_data/qc_qlots_bukola/qc_qlots_byPrimaryDx.pdf", height = 7, wi
   mget(ls(patt = "plotdx_"))
 dev.off()
 
+
+### FROM GitHub: 
+
 ####################
 ## Genotype data ###
 ####################
 
 # Read in VCF
 vcf = readVcf("preprocessed_data/Genotypes/mergedVariants.vcf.gz", "hg38" )
-colnames(vcf) = ss(ss(colnames(vcf), "/", 8), "_")
+colnames(vcf) = ss(basename(colnames(vcf)), "_")
+
+# Match order of RNums 
+all(pd$RNum %in% colnames(vcf))
+vcf = vcf[,pd$RNum] 
+
+# add rs num
+snpMap = import(here("processed-data","02_bulk_qc","common_missense_SNVs_hg38.bed"))
+oo = findOverlaps(query = vcf, subject = snpMap, type="equal")
+rowData(vcf)$snpRsNum = NA
+rowData(vcf)$snpRsNum[queryHits(oo)] = snpMap$name[subjectHits(oo)]
+any(is.na(rowData(vcf)$snpRsNum))
+# FALSE. So we will use them as rownames.
+rownames(vcf) = rowData(vcf)$snpRsNum
+
+## filter ****
+vcf = vcf[info(vcf)$DP > 5*ncol(vcf) & info(vcf)$DP < 80 *ncol(vcf) &
+            nchar(ref(vcf)) == 1 & elementNROWS(alt(vcf)) == 1 &
+            info(vcf)$VDB >0.1,]
+
+## read in obs
+genotyped = readVcf("/dcl01/ajaffe/data/lab/brain_swap/LIBD_Brain_Illumina_h650_1M_Omni5M_Omni2pt5_Macrogen_QuadsPlus_GenotypingBarcode.vcf.gz")
+br = ss(colnames(genotyped),"_")
+pd$hasGenotype = pd$BrNum %in% br
+table(pd$hasGenotype)
+
+pd[!pd$BrNum %in% br,c("BrNum", "RNum")]
+## add genotype info
+
+mm = match(rownames(vcf), rownames(genotyped))
+vcf = vcf[!is.na(mm),]
+genotyped = genotyped[mm[!is.na(mm)],]
+
+## RNA-seq SNPs
+snpsCalled = geno(vcf)$GT
+snpsCalled[snpsCalled == "."] = 0
+snpsCalled[snpsCalled == "0/1"] = 1
+snpsCalled[snpsCalled == "1/1"] = 2
+class(snpsCalled) = "numeric"		  
+
+## DNA genotypes
+snpsGeno = geno(genotyped)$GT
+snpsGeno[snpsGeno == "."] = NA
+snpsGeno[snpsGeno == "0/0"] = 0
+snpsGeno[snpsGeno == "0/1"] = 1
+snpsGeno[snpsGeno == "1/1"] = 2
+class(snpsGeno) = "numeric"
+
+## flip
+table(rowRanges(vcf)$REF == rowRanges(genotyped)$REF  | 
+        rowRanges(vcf)$REF == as.character(unlist(alt(genotyped))) )
+toFlip = which(rowRanges(vcf)$REF != rowRanges(genotyped)$REF)
+snpsGeno[toFlip,] = 2-snpsGeno[toFlip,] 
+
+## correlate
+colnames(snpsGeno)= ss(colnames(snpsGeno), "_")
+snpCorObs = cor(snpsCalled, snpsGeno, use="pairwise.complete.obs")
+
+## check best
+bestCor = data.frame(maxIndex = apply(as.matrix(snpCorObs), 1, which.max),
+                     maxCor = apply(snpCorObs, 1, max, na.rm=TRUE))
+bestCor$RNA_BrNum = pd$BrNum[match(rownames(bestCor), pd$RNum)]
+bestCor$DNA_BrNum = colnames(snpCorObs)[bestCor$maxIndex]
+bestCor$maxIndex = NULL
+bestCor$RNum = rownames(bestCor)
+bestCor$RNA_Region = pd$Region[match(bestCor$RNum, pd$RNum)]
+
+checkCor = bestCor[bestCor$RNA_BrNum != bestCor$DNA_BrNum & bestCor$maxCor > 0.6,]
+checkCor
+
+b = unique(c(checkCor$RNA_BrNum, checkCor$DNA_BrNum))
+bestCor[bestCor$RNA_BrNum %in% b | bestCor$DNA_BrNum %in% b,]
+
+bestCor[bestCor$DNA_BrNum == "Br1750",]
+
+### drop some samples for identity/genotype
+dropRNAs = c("R18355", "R18393", "R18421","R18364")
+pd = pd[! pd$RNum %in% dropRNAs,]
+
+## swap some samples
+pd[pd$RNum == "R18422",4:8] = pheno[pheno$BrNum == "Br1350",4:8]
+pd[pd$RNum == "R18423",4:8] = pheno[pheno$BrNum == "Br1225",4:8]
+
+################################
+############ save counts #######
+################################
+
+## save gene counts
+rse_gene = rse_gene[,pd$RNum]
+colData(rse_gene) = pd
+save(rse_gene, file = paste0("count_data/rse_gene_Roche_Habenula_qcAndAnnotated_n",
+                             ncol(rse_gene), ".Rdata"))
+
+## save exons counts
+rse_exon = rse_exon[,pd$RNum]
+colData(rse_exon) = pd
+save(rse_exon, file = paste0("count_data/rse_exon_Roche_Habenula_qcAndAnnotated_n",
+                             ncol(rse_exon), ".Rdata"))
+
+## save junction counts
+rse_jx = rse_jx[,pd$RNum]
+colData(rse_jx) = pd
+## filter
+jIndex = (rowSums(assays(rse_jx)$counts) > 9) & (rowData(rse_jx)$Class != "Novel")
+rse_jx = rse_jx[jIndex,]
+save(rse_jx, file = paste0("count_data/rse_jx_Roche_Habenula_qcAndAnnotated_n",
+                           ncol(rse_jx), ".Rdata"))
+
+## save tx counts
+rse_tx = rse_tx[,pd$RNum]
+colData(rse_tx) = pd
+save(rse_tx, file = paste0("count_data/rse_tx_Roche_Habenula_qcAndAnnotated_n",
+                           ncol(rse_tx), ".Rdata"))
+
+
+#######################	
+## rank on quality ####
+pdList = split(pd, pd$PrimaryDx)
+lapply(pdList, function(x) x[order(x$totalAssignedGene,decreasing=TRUE)[1:5],c(1:8, 49:54)])
