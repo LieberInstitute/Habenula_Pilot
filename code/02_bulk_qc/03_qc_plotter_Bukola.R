9c86e46b935dae0ff885aac8b58146aea3fd2f20
ajanakub <bukolaajanaku@gmail.com>
2022-11-21 09:45:43 -0500

    Set up new stable variables with computed ribo mito data from build objects files (02_build_objects)

--- code/02_bulk_qc/03_qc_plotter_Bukola.R
+++ code/02_bulk_qc/03_qc_plotter_Bukola.R
@@ -42,10 +42,96 @@ load(here("processed-data", "02_bulk_qc", "count_data_bukola",
           "rse_tx_filt_Roche_Habenula_qcAndAnnotated_n69.Rdata"))
 rse_tx = rse_tx_filt
 
+
 ## Focusing on rse_gene ########################################################
 # Checks:
 table(rse_gene$Flowcell, rse_gene$PrimaryDx) 
 table(rse_gene$Sex, rse_gene$PrimaryDx) 
+
+## Creating Stable Variables ###################################################
+# For ease, all downstream functions will utilize these variables.
+
+# pd & pd_dropped ####
+pd = as.data.frame(colData(rse_gene))
+
+# dropped Race and Sex because all male and all Cauc.
+drop = c("Brain.Region", "FQCbasicStats", "perBaseQual", "perTileQual",
+         "GCcontent", "Ncontent", "SeqLengthDist", "SeqDuplication",
+         "OverrepSeqs", "AdapterContent", "KmerContent", "SeqLength_R1",
+         "perSeqQual", "perBaseContent", names(pd[,grepl("phred", names(pd))]),
+         names(pd[,grepl("Adapter", names(pd))]), "SeqLength_R2", "bamFile",
+         "trimmed", names(pd[,grepl("gene_", names(pd))]), "hasGenotype",
+         "Age", "Race", "subsets_Mito_sum.1", "subsets_Mito_detected.1",
+         "subsets_Mito_percent.1", "subsets_Ribo_sum.1", "subsets_Ribo_detected.1",
+         "subsets_Ribo_percent.1", "sum.1", "detected.1")
+
+pd = pd[,!(names(pd)) %in% drop]
+
+# Changing classes for plotting
+pd$percentGC_R1 <- as.numeric(as.character(pd$percentGC_R1))
+pd$percentGC_R2 <- as.numeric(as.character(pd$percentGC_R2))
+
+# Log10 values
+pd$logNumReads <- log10(pd$numReads)
+pd$logNumMapped <- log10(pd$numMapped)
+pd$logNumUnmapped <- log10(pd$numUnmapped)
+
+# Creating intervals for AgeDeath
+pd$AgeInterval = NA
+levels = quantile(pd$AgeDeath, probs = c(0, 0.25, 0.5, 0.75, 1))
+
+for (i in 1:length(pd$AgeDeath)){
+  if(levels[1] <= pd$AgeDeath[i] && pd$AgeDeath[i] < levels[2]){
+    pd[i, "AgeInterval"] <- "20 to 31"
+  } else if(levels[2] <= pd$AgeDeath[i] && pd$AgeDeath[i] < levels[3]){
+    pd[i, "AgeInterval"] <- "31.5 to 43.5"
+  } else if(levels[3] <= pd$AgeDeath[i] && pd$AgeDeath[i] < levels[4]){
+    pd[i, "AgeInterval"] <- "43.75 to 55.9"
+  } else if(levels[4] <= pd$AgeDeath[i] && pd$AgeDeath[i] <= levels[5]){
+    pd[i, "AgeInterval"] <- "60 to 68"
+  }}
+
+# Variable phenotypes in our data
+phenoCols = as.vector(c("AgeInterval", "PrimaryDx", "Flowcell"))
+## Making phenotypes of interest into factors
+    for (i in phenoCols){
+      pd[,i] <- as.factor(pd[,i])
+    }
+
+# Relevant QC metrics  
+QCmetCols = c("RIN", "percentGC_R1", "percentGC_R2", "ERCCsumLogErr",
+              "numReads", "numMapped", "numUnmapped", "overallMapRate",
+              "concordMapRate", "totalMapped", "mitoMapped", "mitoRate",
+              "totalAssignedGene", "rRNA_rate", "sum", "detected",
+              "subsets_Mito_sum", "subsets_Mito_detected", "subsets_Mito_percent",
+              "subsets_Ribo_sum", "subsets_Ribo_detected", "subsets_Ribo_percent",
+              "logNumReads", "logNumMapped", "logNumUnmapped")
+
+# rename_vars ####
+# Creating df for plot text to rename variables:
+orig_var_name <- c("RNum", "RIN", "BrNum", "AgeDeath", "Sex", "PrimaryDx", 
+                   "percentGC_R1", "percentGC_R2", "ERCCsumLogErr", 
+                   "numReads", "numMapped", "numUnmapped", "overallMapRate", 
+                   "concordMapRate", "totalMapped", "mitoMapped", "mitoRate", 
+                   "totalAssignedGene", "rRNA_rate", "Flowcell", 
+                   "sum", "detected", "subsets_Mito_sum", "subsets_Mito_detected",
+                   "subsets_Mito_percent", "subsets_Ribo_sum", 
+                   "subsets_Ribo_detected", "subsets_Ribo_percent", "logNumReads",
+                   "logNumMapped", "logNumUnmapped", "AgeInterval")
+
+var_plot_title <- c("RNum", "RIN", "Brain Number", "Age of Death", "Sex",
+                    "Primary Dx", "Percent GC R1", "Percent GC R2", 
+                    "ERCC RSS", "Num of Reads", "Num Mapped", "Num Unmapped", 
+                    "Overall Map Rate", "Concordant Map Rate", "Total Mapped", 
+                    "chrM Mapped", "chrM Map Rate", "Total Assigned Genes", 
+                    "Gene rRNA Rate", "Flowcell", "Sum", "Detected", 
+                    "Sum of Mito Subsets", "Detected Mito Subsets", 
+                    "Percent of Mito Subsets", "Sum of Ribo Subsets", 
+                    "Detected Ribo Subsets", "Percent of Ribo Subsets",
+                    "Num Reads (log 10)", "Num Mapped (log 10)", 
+                    "Num Unmapped (log 10)", "Age Intervals")
+
+rename_vars <- data.frame(orig_var_name, var_plot_title)
 
 ### 1. Plotting "qc_plots_by.." ################################################
 # For each QC metric plotted against diagnosis/flowcell and color coded by 
@@ -111,81 +197,6 @@ for(i in covVarInt){
 pdf("preprocessed_data/qc_qlots_bukola/qc_plots_byPrimaryDx.pdf", height = 7, width = 11)
 mget(ls(patt = "plotdx_"))
 dev.off()
-
-#################################
-# Based on smokingMouse pipeline:
-#################################   
-## Stable Variables ############################################################
-# For ease, all downstream functions will utilize these variables.
-
-# pd & pd_dropped ####
-pd = colData(rse)
-pd = as.data.frame(pd)
-# dropped Race and Sex because all male and all Cauc.
-drop = c("Brain.Region", "FQCbasicStats", "perBaseQual", "perTileQual",
-         "GCcontent", "Ncontent", "SeqLengthDist", "SeqDuplication",
-         "OverrepSeqs", "AdapterContent", "KmerContent", "SeqLength_R1",
-         "perSeqQual", "perBaseContent", names(pd[,grepl("phred", names(pd))]),
-         names(pd[,grepl("Adapter", names(pd))]), "SeqLength_R2", "bamFile",
-         "trimmed", names(pd[,grepl("gene_", names(pd))]), "hasGenotype",
-         "Age", "Race")
-pd = pd[,!(names(pd)) %in% drop]
-pd_dropped = colData(rse)[,(names(colData(rse))) %in% drop]
-pd$percentGC_R1 <- as.numeric(pd$percentGC_R1)
-pd$percentGC_R2 <- as.numeric(pd$percentGC_R2)
-
-# Log10 values
-pd$numReads <- log10(pd$numReads)
-pd$numMapped <- log10(pd$numMapped)
-pd$numUnmapped <- log10(pd$numUnmapped)
-
-# QCmetCols ####
-QCmetCols = c("RIN", "percentGC_R1", "percentGC_R2", "ERCCsumLogErr",
-              "numReads", "numMapped", "numUnmapped", "overallMapRate",
-              "concordMapRate", "totalMapped", "mitoMapped", "mitoRate",
-              "totalAssignedGene", "rRNA_rate")
-
-# phenoCols ####
-# Creating intervals for AgeDeath
-pd$AgeInterval = NA
-levels = quantile(pd$AgeDeath, probs = c(0, 0.25, 0.5, 0.75, 1))
-
-for (i in 1:length(pd$AgeDeath)){
-  if(levels[1] <= pd$AgeDeath[i] && pd$AgeDeath[i] < levels[2]){
-    pd[i, "AgeInterval"] <- "20 to 31"
-  } else if(levels[2] <= pd$AgeDeath[i] && pd$AgeDeath[i] < levels[3]){
-    pd[i, "AgeInterval"] <- "31.5 to 43.5"
-  } else if(levels[3] <= pd$AgeDeath[i] && pd$AgeDeath[i] < levels[4]){
-    pd[i, "AgeInterval"] <- "43.75 to 55.9"
-  } else if(levels[4] <= pd$AgeDeath[i] && pd$AgeDeath[i] <= levels[5]){
-    pd[i, "AgeInterval"] <- "60 to 68"
-  }}
-
-phenoCols = as.vector(c("AgeInterval", "PrimaryDx", "Flowcell"))
-
-# updating pd for factors
-for (i in phenoCols){
-  pd[,i] <- as.factor(pd[,i])
-}
-
-# rename_vars ####
-# Creating df for plot text to rename variables:
-orig_var_name <- c("RNum", "RIN", "BrNum", "AgeDeath", "Sex", "PrimaryDx", 
-                   "percentGC_R1", "percentGC_R2", "ERCCsumLogErr", 
-                   "numReads", "numMapped", "numUnmapped", "overallMapRate", 
-                   "concordMapRate", "totalMapped", "mitoMapped", "mitoRate", 
-                   "totalAssignedGene", "rRNA_rate", "Flowcell", "AgeInterval" )
-
-var_plot_title <- c("RNum", "RIN", "Brain Number", "Age oof Death", "Sex",
-                    "Primary Dx", "Percent GC R1", "Percent GC R2", 
-                    "ERCC RSS", "Num of Reads (log 10)", "Num Mapped (log 10)",
-                    "Num Unmapped (log10)", "Overall Map Rate", 
-                    "Concordant Map Rate", "Total Mapped", "chrM Mapped",
-                    "chrM Map Rate", "Total Assigned Genes", "Gene rRNA Rate",
-                    "Flowcell", "Age Intervals")
-
-rename_vars <- data.frame(orig_var_name, var_plot_title)
-
 
 ### 2. Plotting "Mito_vs_Ribo_byPhenotype" #####################################
 # For each QC metric plotted against diagnosis/flowcell and color coded by 
