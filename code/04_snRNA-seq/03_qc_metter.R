08e991348bc57d8a5991576971096f3f32862042
ajanakub <bukolaajanaku@gmail.com>
2023-01-12 11:54:46 -0500

    Identified high mito content via ensDB manner. Will run parallel attempt via secondary workflow

--- code/04_snRNA-seq/03_qc_metter.R
+++ code/04_snRNA-seq/03_qc_metter.R
@@ -9,6 +9,49 @@ library("VariantAnnotation")
 library("here")
 library("ggplot2")
 library("ggrepel")
+library("scater")
+library("batchelor")
+library("scran")
+library("scry")
+library("uwot")
+library("DropletUtils")
+library("Rtsne")
+library("gridExtra")
+library("EnsDb.Hsapiens.v86")
 
 # Loading filtered pre-QC sce object for all 7 Habenula samples
-load(here(""))
+load(here("processed-data", "04_snRNA-seq", "sce_objects", "sce_hb_preQC.Rdata"))
+sce <- sce_hb_preQC
+rm(sce_hb_preQC)
+
+# QC approach based on Workflow 3.3 in OSCA:
+# http://bioconductor.org/books/3.16/OSCA.workflows/unfiltered-human-pbmcs-10x-genomics.html#quality-control-2
+
+# MADs approach for high mito droplets (indicates nuclei where cytoplasm wasn't
+# successfully stripped):
+set.seed(777)
+
+# Prepping sce object to be scanned for mito labels
+rownames(sce) <- uniquifyFeatureNames(rowData(sce)$ID, rowData(sce)$Symbol)
+
+# Defining locations
+location <- mapIds(EnsDb.Hsapiens.v86, keys = rowData(sce)$ID, 
+                   column = "SEQNAME", keytype = "GENEID")
+# Unable to map 2611 of 36601 requested IDs.
+
+# Identifying mito reads
+stats <- perCellQCMetrics(sce, subsets = list(Mito = which(location=="MT")))
+
+# Identifying high mito reads
+high_mito <- isOutlier(stats$subsets_Mito_percent, nmads=3, type="higher")
+table(high_mito)
+    # high_mito
+    # FALSE  TRUE
+    # 17245  2557
+
+
+
+
+
+
+# 
