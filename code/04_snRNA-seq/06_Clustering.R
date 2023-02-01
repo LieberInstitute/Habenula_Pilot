8d2dfe58c0c8ad5a0fbc5c9b74b7674d3ac31b8d
ajanakub <bukolaajanaku@gmail.com>
2023-02-01 15:00:46 -0500

    Ran some clusters, ending here.

--- code/04_snRNA-seq/06_Clustering.R
+++ code/04_snRNA-seq/06_Clustering.R
@@ -24,21 +24,36 @@ sce <- sce_corrbySamp
 rm(sce_corrbySamp)
 
 ##### Approach 1: Louvain Clustering (Steph Hicks) #############################
-firstWay <- buildSNNGraph(sce, k=10, use.dimred = "HARMONY")
+firstWay <- buildSNNGraph(sce, k = 10, use.dimred = "HARMONY")
 lou <- igraph::cluster_louvain(firstWay)
 sce$louvain <- paste0("Louvain", lou$membership)
 table(sce$louvain)
+    # Louvain1 Louvain10 Louvain11 Louvain12 Louvain13 Louvain14  Louvain2  Louvain3 
+    # 1328       754       179      1274       542       893      2043      1485 
+    # Louvain4  Louvain5  Louvain6  Louvain7  Louvain8  Louvain9 
+    # 1436      2277       624      1538      1089      1620
 
 ##### Approach 2: WalkTrap Clustering (Erik) ###################################
 g20 <- buildSNNGraph(sce, k = 20, use.dimred = 'HARMONY')
 clust20 <- igraph::cluster_walktrap(g20)$membership
-colData(sce)$k_20_Erik <- factor(clust20)
-table(colLabels(sce)$k_20_Erik)
+sce$k_20_Erik <- paste0("20wTrap", factor(clust20))
+table(sce$k_20_Erik)
+    # 20wTrap1 20wTrap10 20wTrap11 20wTrap12 20wTrap13 20wTrap14 20wTrap15 20wTrap16 
+    # 1299       127       214      4747       296       192       151       347 
+    # 20wTrap17 20wTrap18 20wTrap19  20wTrap2 20wTrap20 20wTrap21 20wTrap22  20wTrap3 
+    # 211        34        75      1909        86       175        29      1867 
+    # 20wTrap4  20wTrap5  20wTrap6  20wTrap7  20wTrap8  20wTrap9 
+    # 569       450       359      2742       344       859 
 
 g50 <- buildSNNGraph(sce, k=50, use.dimred = 'HARMONY')
 clust50 <- igraph::cluster_walktrap(g50)$membership
-colData(sce)$k_50_Erik <- factor(clust50)
+colData(sce)$k_50_Erik <- paste0("50wTrap", factor(clust50))
 table(colData(sce)$k_50_Erik)
+
+
+### Saving data so far
+save(sce, file = here("processed-data", "04_snRNA-seq", "sce_objects", 
+                              "sce_mid_clustering.Rdata"))
 
 g10 <- buildSNNGraph(sce, k=10, use.dimred = 'HARMONY')
 clust10 <- igraph::cluster_walktrap(g10)$membership
