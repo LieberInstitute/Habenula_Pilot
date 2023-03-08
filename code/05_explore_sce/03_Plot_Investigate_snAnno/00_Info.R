f4f48056d3680ce40186b87734afebd1e037dac7
ajanakub <bukolaajanaku@gmail.com>
2023-03-08 12:12:55 -0500

    Created an explore snAnno folder within explore_sce and also attempted 1vAll expression for each 37 clusters (no longer needed)

---
+++ code/05_explore_sce/03_Plot_Investigate_snAnno/00_Info.R
@@ -0,0 +1,32 @@
+## March 8 2023 - Bukola Ajanaku
+# This folder is solely used to investigate the (18) single nucleus annotation clusters
+# to validate probe their identities and investigate if any further collapses are necessary.
+
+
+      # ## Notes: 3/8/23 with Dr. Maynard:
+      # # 1) Seperate endothelial cells from Astrocytes.
+      # #      a) SLC14A1 is endo
+      # #      b) AC073941.1 is not a really annotated gene (pseudogene)
+      # #      c) first two are astro
+      # # 2) Genes  that we are going to rrender from Excit.Thal top 5 may not be specifically 
+      # #      thelamus genes because the excitatory aspect may throw off the comparison.
+      # # 3) For Hb top 5 markers:
+      # #       a) LHX1 seems to be in the thalamus (a marker of the zona limitans)
+      # #      b) MAB21L2 seems to be in the thalamus 
+      # #      c) BarHl1 
+      # #      d) 
+      # #      
+      # #  4) Inhib.Thal
+      # #      a) KIT is expressed in PVALV 
+      # #      
+      # # * Probe the Hb cluster with Habenula and Thalamus markers 
+      # 
+      # ## 1) Make violin plots for combined clusters 
+      # ## 2) Create heatmap that doesn't split by Sample. 
+      # ## 3) Hockey Stick plots (from Zoom meeting with Louise slacked in Habenula chat.)
+      # ## 4) VP1: Grab list of genes from Dr. Maynard for violin plots in Hb cells (violin plots)
+      # ## 5) VP2: Make violin plots for snAnno based on my original markers.custom
+      # ## 6) VP3: Merge MHB 2 and 3 to see what we get (1 and 4 look very specific) and re-run mean ratio expression.
+      # ## 7) VP4: Do regular thalamic and habenula genes on Hb cluster violin plots.
+
+# 
