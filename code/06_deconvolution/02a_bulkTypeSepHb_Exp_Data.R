d8a5971c751a30516536f8a3853698179fe814c1
ajanakub <bukolaajanaku@gmail.com>
2023-03-17 15:30:26 -0400

    Running function across all bulk collapses

--- code/06_deconvolution/02a_bulkTypeSepHb_Exp_Data.R
+++ code/06_deconvolution/02a_bulkTypeSepHb_Exp_Data.R
@@ -7,6 +7,7 @@ library("here")
 library("tidyverse")
 library("sessioninfo")
 library("DeconvoBuddies")
+library("ggplot2")
 
 # loading regular sce object
 load(here("processed-data", "06_deconvolution", "sce_objects", "sce_first_bulkTypes.RDATA"))
@@ -46,6 +47,8 @@ load(here("processed-data", "06_deconvolution", "sce_objects", "sce_first_bulkTy
 marker_stats_SepHb <- left_join(mean_ratio_SepHb, findMark_SepHb, by = c("gene", "cellType.target"), 
                                 multiple = "all")
 
+######## PLOTTING #######
+## mean expression plots 
 pdf(file = here(plot_dir, "marker_stats_SepHb_top5_meanExp.pdf"))
 for (j in levels(as.factor(sce$bulkTypeSepHb))) {
   message(j)
@@ -61,7 +64,21 @@ for (j in levels(as.factor(sce$bulkTypeSepHb))) {
 }
 dev.off()
 
-# Saving 
+## hockey stick plots (mean expression vs 1vAll expression)
+# adding color group
+marker_stats_SepHb$Top25 <- "No"
+marker_stats_SepHb[which(marker_stats_SepHb$rank_ratio <= 25),
+                   "Top25"] <- "Yes"
+
+pdf(file = here(plot_dir, "marker_stats_SepHb_top5_hockeystick.pdf"))
+  
+ggplot(marker_stats_SepHb, aes(ratio, std.logFC)) +
+  geom_point(size = 0.5, aes(colour = Top25)) +  
+  facet_wrap(~cellType.target, scales = "free") 
+
+dev.off()
+
+######## SAVING #######
 save(marker_stats_SepHb, mean_ratio_SepHb, findMark_SepHb, file =  here(new_dir, "bulkTypeSepHb_exp.Rdata"))
 
 
--- code/06_deconvolution/02c_bulkTypeAllCollapse_Exp_Data.R
+++ code/06_deconvolution/02c_bulkTypeAllCollapse_Exp_Data.R
@@ -1,3 +1,80 @@
 ## March 17, 2023 - Bukola Ajanaku
 # Running mean expression and 1 vs All expression methods on bulkTypeAllCollapse.
 # qrsh -l mem_free=20G,h_vmem=20G
+
+library("SingleCellExperiment")
+library("here")
+library("tidyverse")
+library("sessioninfo")
+library("DeconvoBuddies")
+library("ggplot2")
+
+# loading regular sce object
+load(here("processed-data", "06_deconvolution", "sce_objects", "sce_first_bulkTypes.RDATA"))
+
+# creating directories for saving  
+## output save directory
+new_dir <- here("processed-data", "06_deconvolution", "sce_objects", "expression_data")
+## plotting directory
+plot_dir <- here("plots", "06_deconvolution", "expression_data")
+
+
+##### Running expression code! #####
+# mean ratio
+mean_ratio_AllCollapse <- get_mean_ratio2(
+  sce,
+  cellType_col = "bulkTypeAllCollapse",
+  assay_name = "logcounts",
+  add_symbol = TRUE
+)
+
+# 1vAll Expression
+findMark_AllCollapse <- findMarkers_1vAll(
+  sce,
+  assay_name = "counts",
+  cellType_col = "bulkTypeAllCollapse",
+  add_symbol = FALSE,
+  mod = "~Sample"
+)
+
+## For combined manually annotated clusters 
+marker_stats_AllCollapse <- left_join(mean_ratio_AllCollapse, findMark_AllCollapse, by = c("gene", "cellType.target"), 
+                                  multiple = "all")
+
+######## PLOTTING #######
+## mean expression plots 
+pdf(file = here(plot_dir, "marker_stats_AllCollapse_top5_meanExp.pdf"))
+for (j in levels(as.factor(sce$bulkTypeAllCollapse))) {
+  message(j)
+  
+  print(plot_marker_express(sce, 
+                            marker_stats_AllCollapse, 
+                            n_genes = 5,
+                            rank_col = "rank_ratio", 
+                            anno_col = "anno_ratio",
+                            cellType_col = "bulkTypeAllCollapse",
+                            cell_type = j)
+  )
+}
+dev.off()
+
+## hockey stick plots (mean expression vs 1vAll expression)
+# adding color group
+marker_stats_AllCollapse$Top25 <- "No"
+marker_stats_AllCollapse[which(marker_stats_AllCollapse$rank_ratio <= 25),
+                     "Top25"] <- "Yes"
+
+pdf(file = here(plot_dir, "marker_stats_AllCollapse_top5_hockeystick.pdf"))
+
+ggplot(marker_stats_AllCollapse, aes(ratio, std.logFC)) +
+  geom_point(size = 0.5, aes(colour = Top25)) +  
+  facet_wrap(~cellType.target, scales = "free") 
+
+dev.off()
+
+######## SAVING #######
+save(marker_stats_AllCollapse, mean_ratio_AllCollapse, findMark_AllCollapse, file =  here(new_dir, "bulkTypeAllCollapse_exp.Rdata"))
+
+
+
+
