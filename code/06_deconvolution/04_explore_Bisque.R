2ade00a85d35f2aa1183f5fa722480ef479000c3
ajanakub <bukolaajanaku@gmail.com>
2023-04-17 10:31:27 -0400

    Re-arrange expression plots of habenula per sample
    Co-authored-by: Louise Huuki <lahuuki@gmail.com>

--- code/06_deconvolution/04_explore_Bisque.R
+++ code/06_deconvolution/04_explore_Bisque.R
@@ -6,9 +6,7 @@ library(SummarizedExperiment)
 library(here)
 library(SingleCellExperiment)
 library(DeconvoBuddies)
-library(dplyr)
-library(tibble)
-library(ggplot2)
+library(tidyverse)
 
 # loading sce object (sn data) post drop of Hb cluster! 
 load(here("processed-data", "06_deconvolution", "sce_objects", "sce_first_bulkTypes.RDATA"),
@@ -55,25 +53,38 @@ prop_long <- est_prop$bulk.props |>
   as.data.frame() |>
   rownames_to_column("Sample") |>
   tidyr::pivot_longer(!Sample, names_to = "cellType", values_to = "prop") |>
-  left_join(pd) 
+  left_join(pd) |>
+  mutate(factor_CT = factor(cellType, levels = 
+                              c("Astrocyte", "Endo", "Microglia", "Oligo", "OPC",
+                                "Inhib.Thal", "Excit.Thal" , "MHb", "LHb")) ) |>
+  arrange(factor_CT)
 
-prop_long$HbSum <- NA
+sum_Prop <- prop_long |>
+  filter(cellType %in% c("LHb", "MHb")) |>
+  group_by(BrNum) |>
+  summarize(Hb_sum = sum(prop)) |>
+  mutate(Br_Order = fct_reorder(BrNum, Hb_sum)) |>
+  arrange(Br_Order)
 
-# creating a column to organize plot by Hb sum.
-for(i in unique(prop_long$BrNum)) {
-  prop_sub <- prop_long[prop_long$BrNum == i,] 
-  HbSummer <- sum(prop_sub[prop_sub$cellType %in% c("MHb", "LHb"), ]$prop)
-  prop_long[prop_long$BrNum == i,]$HbSum <- HbSummer
-}
+prop_long <- left_join(prop_long, sum_Prop) |>
+  arrange(Br_Order) 
 
-prop_long$HbSum <- as.factor(prop_long$HbSum)
+# prop_long$HbSum <- NULL
+# 
+# # creating a column to organize plot by Hb sum.
+# for(i in unique(prop_long$BrNum)) {
+#   prop_sub <- prop_long[prop_long$BrNum == i,] 
+#   HbSummer <- sum(prop_sub[prop_sub$cellType %in% c("MHb", "LHb"), ]$prop)
+#   prop_long[prop_long$BrNum == i,]$HbSum <- HbSummer
+# }
+# 
+# prop_long$HbSum <- as.factor(prop_long$HbSum)
 
 ## create composition bar plots
-pdf(here(plot_dir, "composition_bar_plot_bulkTypeSepHb_3.pdf"), width = 20, height = 10)
-  plot_composition_bar(prop_long = prop_long, sample_col = "Sample",
-                        x_col = "BrNum", ct_col = "cellType") + 
-    scale_fill_manual(values = color_bulk_clusters) +
-    aes(x = sort(prop_long$HbSum, decreasing = FALSE))
+pdf(here(plot_dir, "composition_bar_plot_bulkTypeSepHb_5.pdf"), width = 21, height = 12)
+  plot_composition_bar(prop_long = prop_long, sample_col = "Br_Order",
+                        x_col = "Br_Order", ct_col = "factor_CT") + 
+    scale_fill_manual(values = color_bulk_clusters) 
 dev.off()
 
 
