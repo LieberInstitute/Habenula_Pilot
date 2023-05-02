4cffa9d219df5004eef309e8b52f3f4673ef191f
ajanakub <bukolaajanaku@gmail.com>
2023-05-02 15:23:11 -0400

    Working on snRNAseq cell type expression proportion plots

---
+++ code/99_paper_figs/08_sce_Plot_Expression.R
@@ -0,0 +1,52 @@
+## May 2, 2023 - Bukola Ajanaku
+# Plotting cell-type expression pre and post drop per sample 
+# qrsh -l mem_free=20G,h_vmem=20G
+
+# loading relevant libraries
+library(SummarizedExperiment)
+library(here)
+library(SingleCellExperiment)
+library(DeconvoBuddies)
+library(tidyverse)
+
+# loading sce object with dropped ambig cluster
+load(file = here("processed-data", "99_paper_figs", "sce_objects", 
+                 "official_final_sce.RDATA"))
+
+# dropping OPC_noisy
+table(sce$OPC_clean)
+    # No   Yes 
+    # 594 16437 
+sce <- sce[, which(sce$OPC_clean == "Yes")]
+# check again
+table(sce$OPC_clean)
+    # Yes 
+    # 16437 
+
+# grabbing relevant columns
+pd <- colData(sce)[,c("Sample", "final_Annotations", "NeuN", "Run")]
+pd$Prop <- NA
+
+# creating function that summarizes data
+for(i in unique(sce$Sample)){
+  pD <- pd[pd$Sample == i, ]
+  
+  # make sure each nuclei is unique
+  if(any(duplicated(rownames(pD)))){
+    return(print("ERROR: Rownames are not unique!"))
+  }
+  
+  # grab the total number of nuclei per Sample
+  total_nuclei <- nrow(pD)
+  
+  # running through each cluster
+  for(n in unique(pD$final_Annotations)){
+    pD <- pD[pD$final_Annotations == n, ]
+    
+    # calculating proportion for given sample in respective cluster
+    pD$Prop <- nrow(pD) / total_nuclei
+  }
+  
+}
+
+table(sce$final_Annotations)
