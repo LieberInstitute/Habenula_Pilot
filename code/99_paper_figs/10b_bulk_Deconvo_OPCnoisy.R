402634b773836ef3e5927aa75e4e9dc288f06f42
ajanakub <bukolaajanaku@gmail.com>
2023-05-03 13:08:53 -0400

    Ran bulk deconvo using both methods

--- code/99_paper_figs/10b_bulk_Deconvo_OPCnoisy.R
+++ code/99_paper_figs/10b_bulk_Deconvo_OPCnoisy.R
@@ -10,6 +10,7 @@ library(jaffelab)
 library(dplyr)
 library(BisqueRNA)
 library(ggplot2)
+library(tidyverse)
 
 # loading final sce object 
 load(file = here("processed-data", "99_paper_figs", "sce_objects", 
@@ -84,8 +85,83 @@ plot_marker_express_ALL(sym_sce,
 )
 
 
+## copied directly from run_Bisque.R file in the bulk deconvo folder 
+## creating marker_list of top 25 genes
+marker_genes <- marker_stats |>
+  filter(rank_ratio <= 25, gene %in% rownames(rse_gene)) |>
+  pull(gene)
+
+length(marker_genes)
+# [1] 170
+
+##### Running BISQUE ###########################################################
+exp_set_bulk <- Biobase::ExpressionSet(assayData = assays(rse_gene[marker_genes,])$counts,
+                                       phenoData=AnnotatedDataFrame(
+                                         as.data.frame(colData(rse_gene))[c("BrNum")]))
+
+exp_set_sce <- Biobase::ExpressionSet(assayData = as.matrix(assays(sym_sce[marker_genes,])$counts),
+                                      phenoData=AnnotatedDataFrame(
+                                        as.data.frame(colData(sym_sce))[,c("bulkTypeSepHb","Sample")]))
+
+# checking for nuclei with 0 marker expression
+zero_cell_filter <- colSums(exprs(exp_set_sce)) != 0
+message("Exclude ", sum(!zero_cell_filter), " cells")
+# Exclude 0 cells [yayyyyy!!!!]
+
+est_prop <- ReferenceBasedDecomposition(bulk.eset = exp_set_bulk,
+                                        sc.eset = exp_set_sce,
+                                        cell.types = "bulkTypeSepHb",
+                                        subject.names = "Sample",
+                                        use.overlap = FALSE)
+
+#### grabbed from my exploreBisque.R file in the bulk deconvo folder
+# custom color scheme
+color_bulk_clusters <- 
+  c( "Oligo" = c("#A9A9A9"), # dark grey
+     "OPC"= c("#7393B3"), # blue grey
+     "Microglia" = c("#E5E4E2"), # platinum
+     "Astrocyte" = c("#36454F"), # ash grey
+     "Endo" = c("#848884"), # smoke
+     "Inhib.Thal" = c('#2AAA8A'), # jungle green
+     "Excit.Thal" = c("#478778"), # lincoln green
+     "LHb" = c("#DE3163"), # cerise
+     "MHb" = c("#00FFFF") # aqua
+  )
+
+# grabbing relevant phenotype info for bulk data
+pd <- colData(rse_gene) |>
+  as.data.frame() |>
+  select(Sample = RNum, BrNum, PrimaryDx)
+
+est_prop$bulk.props <- t(est_prop$bulk.props)
+head(est_prop$bulk.props)
+
+prop_long <- est_prop$bulk.props |>
+  as.data.frame() |>
+  rownames_to_column("Sample") |>
+  tidyr::pivot_longer(!Sample, names_to = "cellType", values_to = "prop") |>
+  left_join(pd) |>
+  mutate(factor_CT = factor(cellType, levels = 
+                              c("Astrocyte", "Endo", "Microglia", "Oligo", "OPC",
+                                "Inhib.Thal", "Excit.Thal" , "MHb", "LHb")) ) |>
+  arrange(factor_CT)
+
+sum_Prop <- prop_long |>
+  filter(cellType %in% c("LHb", "MHb")) |>
+  group_by(BrNum) |>
+  summarize(Hb_sum = sum(prop)) |>
+  mutate(Br_Order = fct_reorder(BrNum, Hb_sum)) |>
+  arrange(Br_Order)
+
+prop_long <- left_join(prop_long, sum_Prop) |>
+  arrange(Br_Order) 
 
 
-
+## create composition bar plots
+pdf(here(plot_dir, "bulk_Deconvo_Composition_OPC_noisy.pdf"), width = 21, height = 12)
+plot_composition_bar(prop_long = prop_long, sample_col = "Br_Order",
+                     x_col = "Br_Order", ct_col = "factor_CT") + 
+  scale_fill_manual(values = color_bulk_clusters) 
+dev.off()
 
 # 
