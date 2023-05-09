22e90117ab4e4ccb4d5c5adff6377a15d7a350dd
ajanakub <bukolaajanaku@gmail.com>
2023-05-08 20:12:53 -0400

    Created different versions of the composition plots

--- code/99_paper_figs/08_sce_Plot_Expression.R
+++ code/99_paper_figs/08_sce_Plot_Expression.R
@@ -30,7 +30,29 @@ if(!dir.exists(plot_dir)){
 }
 
 # creating cell-type colors 
+# adding color pallete (same color scheme used for progress report heatmap)
+cluster_colors <- c( "Oligo" = c("#ba6425"), 
+                     "OPC"= c("#987020"), 
+                     "OPC_noisy" = c("#bf9146"),
+                     "Microglia" = c("#5e0056"), 
+                     "Astrocyte" = c("#e2693e"), 
+                     "Endo" = c("#e295de"), 
+                     "Excit.Neuron" = c("#709438"), 
+                     "Inhib.Thal" = c("#eed7a1"),  
+                     "Excit.Thal" = c('#E1F8DC'), 
+                     "LHb.1" = c("#00FFFF"),
+                     "LHb.2" = c("#0096FF"), 
+                     "LHb.3" = c ("#1434A4"), 
+                     "LHb.4" = c("#00008B"), 
+                     "LHb.5" = c("#40E0D0"), 
+                     "LHb.6" = c("#008080"),  
+                     "LHb.7" = c("#7DF9FF"), 
+                     "MHb.1" = c("#800020"), 
+                     "MHb.2" = c("#D70040"),
+                     "MHb.3" = c("#D2042D") 
+)
 
+####### FINAL ANNOTATIONS LEVEL #################################################
 #### get proportions before dropping ambig #####################################
 colData(sce)[, c("final_Annotations", "Sample", "NeuN")]
 
@@ -72,12 +94,14 @@ comp_plot_both <- ggplot(data = prop_ambig_plus, aes(x = Sample, y = prop, fill 
   geom_bar(stat = "identity") +
   geom_text(
     aes(
-      label = ifelse(prop > 0.02, format(round(prop, 3), 3), ""),
+      label = ifelse(prop > 0.02, format(round(prop, 3), 3), "")
     ),
     size = 2.5,
     position = position_stack(vjust = 0.5),
     color = "black"
   ) +
+  scale_fill_manual(values = c(cluster_colors)) +
+  scale_color_manual(values = c(`TRUE` = "white", `FALSE` = "black")) +
   labs(y = "Proportion", fill = "Cell Type") +
   facet_grid(fct_rev(ambig) ~ NeuN, scales = "free", space = "free") +
   
@@ -85,9 +109,93 @@ comp_plot_both <- ggplot(data = prop_ambig_plus, aes(x = Sample, y = prop, fill 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   guides(color = FALSE, fill = guide_legend(ncol = 1))
 
-pdf(file = here(plot_dir, "full_Comp_Express_Plot.pdf"))
+pdf(file = here(plot_dir, "full_Comp_Express_Plot_finalAnnoLEVEL.pdf"), width = 7, height = 11)
+comp_plot_both 
+dev.off()
+
+####### BULK COLLAPSE LEVEL ####################################################
+cluster_colors_bulk <- c("Oligo" = c("#ba6425"), 
+                     "OPC"= c("#987020"), 
+                     "OPC_noisy" = c("#A9A9A9"),
+                     "Microglia" = c("#1c0f77"), 
+                     "Astrocyte" = c("#4e8f39"), 
+                     "Endo" = c("#e295de"), 
+                     "Excit.Neuron" = c("#71797E"), 
+                     "Inhib.Thal" = c("#eed7a1"),  
+                     "Excit.Thal" = c('#E1F8DC'), 
+                     "LHb" = c("#00FFFF"),
+                     "MHb" = c("#D70040")
+)
+
+# Combining into 5 glia, two thalamus, 1 broad LHb, and 1 broad MHb.
+sce$bulkTypeSepHb <- sce$final_Annotations
+
+# collapsed on into bulk annotations
+sce$bulkTypeSepHb[sce$bulkTypeSepHb %in% grep("^LHb\\.", unique(sce$bulkTypeSepHb), value = TRUE)] <-  'LHb'
+sce$bulkTypeSepHb[sce$bulkTypeSepHb %in% grep("^MHb\\.", unique(sce$bulkTypeSepHb), value = TRUE)] <-  'MHb'
+
+#### get proportions before dropping ambig #####################################
+prop_dirty <- as.data.frame(colData(sce)[, 
+                                c("bulkTypeSepHb", "Sample", "NeuN")]) |>
+  group_by(Sample, bulkTypeSepHb, NeuN) |>
+  summarize(n = n()) |>
+  group_by(Sample) |>
+  mutate(prop = n / sum(n))
+
+# dropping the clusters we dropped
+sce <- sce[, sce$bulkTypeSepHb != "OPC_noisy"]
+sce <- sce[, sce$bulkTypeSepHb != "Excit.Neuron"]
+
+#### proportions of nuclei using post-drop information #########################
+pd <- as.data.frame(colData(sce))
+table(pd$bulkTypeSepHb)
+    # Astrocyte       Endo Excit.Thal Inhib.Thal        LHb        MHb  Microglia 
+    # 538         38       1800       7612       2214        710        145 
+    # Oligo        OPC 
+    # 2178       1202 
+
+prop_clean <- pd[,c("bulkTypeSepHb", "Sample", "NeuN")] |>
+  group_by(Sample, bulkTypeSepHb, NeuN) |>
+  summarize(n = n()) |>
+  group_by(Sample) |>
+  mutate(prop = n / sum(n))
+
+
+### combines prop_dirty and prop_clean
+prop_ambig_plus <- prop_dirty |>
+  mutate(ambig = "Pre-drop") |>
+  bind_rows(prop_clean |> mutate(ambig = "Post-drop"))
+
+# plots composition plot using prop_clean and prop_dirty
+comp_plot_both <- ggplot(data = prop_ambig_plus, aes(x = Sample, 
+                              y = prop, fill = bulkTypeSepHb)) +
+  geom_bar(stat = "identity") +
+  geom_text(
+    aes(
+      label = ifelse(prop > 0.02, format(round(prop, 3), 3), "")
+    ),
+    size = 2.5,
+    position = position_stack(vjust = 0.5),
+    color = "black"
+  ) +
+  scale_fill_manual(values = c(cluster_colors_bulk)) +
+  scale_color_manual(values = c(`TRUE` = "white", `FALSE` = "black")) +
+  labs(y = "Proportion", fill = "Cell Type") +
+  facet_grid(fct_rev(ambig) ~ NeuN, scales = "free", space = "free") +
+  
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  guides(color = FALSE, fill = guide_legend(ncol = 1))
+
+pdf(file = here(plot_dir, "full_Comp_Express_Plot_bulkAnnoLEVEL.pdf"), width = 7, height = 11)
 comp_plot_both 
 dev.off()
 
 
-#  
+
+
+
+
+
+
+# 
