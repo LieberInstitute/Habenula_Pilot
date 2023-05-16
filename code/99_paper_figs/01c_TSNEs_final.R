44a193e33c75c6d93d48b4e30e51a5ff307bc731
ajanakub <bukolaajanaku@gmail.com>
2023-05-16 14:38:14 -0400

    Updated first two final TSNE plots on paper

--- code/99_paper_figs/01c_TSNEs_final.R
+++ code/99_paper_figs/01c_TSNEs_final.R
@@ -11,9 +11,15 @@ library("ggplot2")
 library("cowplot")
 
 
-# Loading sce object
+# loading pre-harmony sce object 
+load(here("processed-data", "04_snRNA-seq", "sce_objects", 
+          "sce_uncorrected_PCA.Rdata"), verbose = TRUE)
+  # sce_uncorrected
+
+# Loading post-harmony sce object
 load(file = here("processed-data", "99_paper_figs", "sce_objects", 
                  "sce_final_preHbdrop.RDATA"), verbose = TRUE)
+  # sce 
 
 # sourcing official color palette 
 source(file = here("code", "99_paper_figs", "source_colors.R"))
@@ -35,7 +41,7 @@ table(sce$final_Annotations)
   # OPC_noisy 
   # 594
 
-sce_old <- sce 
+sce_dirty <- sce 
 
 # cleaning sce object
 sce <- sce[, sce$final_Annotations != "OPC_noisy"]
@@ -50,32 +56,78 @@ table(sce$final_Annotations)
   # 145       2178       1202 
 
 
+# TSNE pre and post harmony by Sample 
 pdf(here(plot_dir, "TSNE_PrePostHarmony_bySample.pdf"), 
-    width = 14, height = 9)
+    width = 11, height = 9)
 
-    plot1_old <- plotReducedDim(sce_old, dimred = "TSNE") +
-      geom_point(aes(color = sce_old$Sample)) + 
-      theme(legend.position = "none")
+    plot1_unc <- plotReducedDim(sce_uncorrected, dimred = "TSNE") +
+      geom_point(aes(color = sce_uncorrected$Sample)) + 
+      theme(legend.position = "none") + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
     
-    plot2_old <- plotReducedDim(sce_old, dimred = "TSNE") +
-      geom_point(aes(color = sce_old$Sample)) + 
-      facet_wrap(~ sce_old$Sample) + 
-      guides(color = guide_legend(title="Sample ID"))
+    plot2_unc <- plotReducedDim(sce_uncorrected, dimred = "TSNE") +
+      geom_point(aes(color = sce_uncorrected$Sample)) + 
+      facet_wrap(~ sce_uncorrected$Sample) + 
+      guides(color = guide_legend(title="Sample ID")) + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
     
     plot1 <- plotReducedDim(sce, dimred = "TSNE") +
       geom_point(aes(color = sce$Sample)) + 
-      theme(legend.position = "none")
+      theme(legend.position = "none") + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
     
     plot2 <- plotReducedDim(sce, dimred = "TSNE") +
       geom_point(aes(color = sce$Sample)) + 
       facet_wrap(~ sce$Sample) + 
-      guides(color = guide_legend(title="Sample ID"))
+      guides(color = guide_legend(title="Sample ID")) + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
 
-  plot_grid(plot1_old, plot2_old, plot1, plot2,
-            nrow = 2)
+  plot_grid(plot1_unc, plot2_unc, plot1, plot2,
+            nrow = 2,
+            labels = c("A", "", "B", ""))
 dev.off()
 
+# TSNE pre and post harmony colored by Sample, faceted by Run
+# changing Run Labels 
+sce_uncorrected$Run <- recode(sce_uncorrected$Run,
+                              "1" = "Run 1",
+                              "2" = "Run 2",
+                              "3" = "Run 3")
+sce$Run <- recode(sce$Run,
+                  "1" = "Run 1",
+                  "2" = "Run 2",
+                  "3" = "Run 3")
 
+pdf(here(plot_dir, "TSNE_PrePostHarmony_bySample_facetbyRun.pdf"), 
+    width = 16, height = 9)
+
+    plot1_unc <- plotReducedDim(sce_uncorrected, dimred = "TSNE") +
+      geom_point(aes(color = sce_uncorrected$Sample)) + 
+      theme(legend.position = "none") + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
+    
+    plot2_unc <- plotReducedDim(sce_uncorrected, dimred = "TSNE") +
+      geom_point(aes(color = sce_uncorrected$Sample)) + 
+      facet_wrap(~ sce_uncorrected$Run) + 
+      guides(color = guide_legend(title="Sample ID")) + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
+    
+    plot1 <- plotReducedDim(sce, dimred = "TSNE") +
+      geom_point(aes(color = sce$Sample)) + 
+      theme(legend.position = "none") + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
+    
+    plot2 <- plotReducedDim(sce, dimred = "TSNE") +
+      geom_point(aes(color = sce$Sample)) + 
+      facet_wrap(~ sce$Run) + 
+      guides(color = guide_legend(title="Sample ID")) + 
+      labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
+
+  plot_grid(plot1_unc, plot2_unc, plot1, plot2,
+            nrow = 2,
+            labels = c("A", "", "B", ""),
+            rel_widths = c(.3, .6))
+dev.off()
 
 
 
