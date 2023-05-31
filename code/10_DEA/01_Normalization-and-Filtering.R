d11e1133f779e1fa6d4a31a86c37c0b2529b0150
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-05-31 10:02:46 -0600

    Started script to normalize gene counts and filter features

---
+++ code/10_DEA/01_Normalization-and-Filtering.R
@@ -0,0 +1,62 @@
+library("here")
+library("SummarizedExperiment")
+library("edgeR")
+library("limma")
+library("sessioninfo")
+
+
+
+############################# Load rse gene object ############################
+
+load(
+    here(
+        "preprocessed_data",
+        "count_data_bukola",
+        "rse_gene_Roche_Habenula_qcAndAnnotated_n69.Rdata"
+    ),
+    verbose = TRUE
+)
+
+lobstr::obj_size(rse_gene)
+# 40.63 MB
+
+class(rse_gene)
+# [1] "RangedSummarizedExperiment"
+# attr(,"package")
+# [1] "SummarizedExperiment"
+
+rse_gene
+# class: RangedSummarizedExperiment
+# dim: 58037 69
+# metadata(0):
+# assays(1): counts
+# rownames(58037): ENSG00000223972.5 ENSG00000227232.5 ...
+#   ENSG00000210195.2 ENSG00000210196.2
+# rowData names(10): Length gencodeID ... NumTx gencodeTx
+# colnames: NULL
+# colData names(68): RNum RIN ... Flowcell hasGenotype
+
+dim(colData(rse_gene))
+# [1] 69 68
+
+colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
+colData(rse_gene)$log10_library_size <- log10(colData(rse_gene)$library_size)
+colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x) {
+    length(x[which(x > 0)])
+})
+
+###############################################################################
+
+
+######################### Reproducibility information #########################
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+###############################################################################
+
+
