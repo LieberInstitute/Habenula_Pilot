d4a1a6ea1df2cb52180286f52fa048fcc13d72f5
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-02 09:59:27 -0600

    identify variance explained by qc_metrics

--- code/10_DEA/VariancePartition.R
+++ code/10_DEA/VariancePartition.R
@@ -36,6 +36,24 @@ class(rse_gene_filt)
 
 
 
+############################## Variance explained #############################
+
+## Set up qc_metrics and colors for the plot
+qc_metrics <- c("mitoRate", "rRNA_rate", "overallMapRate", "totalAssignedGene", "concordMapRate", "log10_library_size", "detected_num_genes", "RIN")
+colors=c("mitoRate"="turquoise4", "rRNA_rate" = "bisque2", "overallMapRate"="indianred1", "totalAssignedGene" = "blueviolet", "concordMapRate"="lightsalmon", "log10_library_size"="palegreen3", "detected_num_genes"="skyblue2", "RIN"="blue3")
+
+exp_vars<-getVarianceExplained(rse_gene_filt, variables=qc_metrics, exprs_values = "logcounts")
+
+## Plot density graph for each variable
+varience_plot <- plotExplanatoryVariables(exp_vars, theme_size = 12, nvars_to_plot = Inf)
+varience_plot <- varience_plot + scale_colour_manual(values = colors) +
+    labs(color="Variables")
+
+ggsave(filename = here("plots/10_DEA/ExplanatoryVars.pdf"), varience_plot, width = 35, height = 25, units = "cm")
+
+###############################################################################
+
+
 ######################### Reproducibility information #########################
 
 ## Reproducibility information
