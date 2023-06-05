a7a43705821d330e58d23acff78b7586e8d5067a
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-05 13:39:24 -0600

    Variance partition and styling Variance partition is based on https://github.com/LieberInstitute/fentanyl_rat_hb_amy/blob/ec13c42be48fa2aeb6841709aba8b14247a86e67/code/04_EDA/03_Explore_gene_level_effects.R#L219-L249
    Co-authored-by: Daianna Gonzalez-Padilla <glezdaianna@gmail.com>

--- code/10_DEA/03_VariancePartition.R
+++ code/10_DEA/03_VariancePartition.R
@@ -40,14 +40,14 @@ class(rse_gene_filt)
 
 ## Set up qc_metrics and colors for the plot
 qc_metrics <- c("mitoRate", "rRNA_rate", "overallMapRate", "totalAssignedGene", "concordMapRate", "log10_library_size", "detected_num_genes", "RIN")
-colors=c("mitoRate"="turquoise4", "rRNA_rate" = "bisque2", "overallMapRate"="indianred1", "totalAssignedGene" = "blueviolet", "concordMapRate"="lightsalmon", "log10_library_size"="palegreen3", "detected_num_genes"="skyblue2", "RIN"="blue3")
+colors <- c("mitoRate" = "turquoise4", "rRNA_rate" = "bisque2", "overallMapRate" = "indianred1", "totalAssignedGene" = "blueviolet", "concordMapRate" = "lightsalmon", "log10_library_size" = "palegreen3", "detected_num_genes" = "skyblue2", "RIN" = "blue3")
 
-exp_vars<-getVarianceExplained(rse_gene_filt, variables=qc_metrics, exprs_values = "logcounts")
+exp_vars <- getVarianceExplained(rse_gene_filt, variables = qc_metrics, exprs_values = "logcounts")
 
 ## Plot density graph for each variable
 varience_plot <- plotExplanatoryVariables(exp_vars, theme_size = 12, nvars_to_plot = Inf)
 varience_plot <- varience_plot + scale_colour_manual(values = colors) +
-    labs(color="Variables")
+    labs(color = "Variables")
 
 ggsave(filename = here("plots/10_DEA/ExplanatoryVars.pdf"), varience_plot, width = 35, height = 25, units = "cm")
 
@@ -62,14 +62,39 @@ formula <- ~ mitoRate + rRNA_rate + overallMapRate + totalAssignedGene + concord
 corpairs <- canCorPairs(formula, colData(rse_gene_filt))
 
 pheatmap(
-        corpairs,
-        color = hcl.colors(50, "YlOrRd", rev = TRUE),
-        fontsize = 8,
-        border_color = "black",
-        height = 6,
-        width = 6.5,
-        filename = here("plots/10_DEA/CCA_heatmap.pdf")
-    )
+    corpairs,
+    color = hcl.colors(50, "YlOrRd", rev = TRUE),
+    fontsize = 8,
+    border_color = "black",
+    height = 6,
+    width = 6.5,
+    filename = here("plots/10_DEA/CCA_heatmap.pdf")
+)
+
+###############################################################################
+
+
+
+############################## Variance partition #############################
+
+genes_var_zero <- which(apply(assays(rse_gene_filt)$logcounts, 1, var) == 0)
+if (length(genes_var_zero) > 0) {
+    rse_gene_filt <- rse_gene_filt[-genes_var_zero, ]
+}
+
+formula <- ~ mitoRate + rRNA_rate + overallMapRate + totalAssignedGene + concordMapRate + log10_library_size + detected_num_genes + RIN
+
+## Loop over each gene to fit model and extract variance explained by each variable
+varPart <- fitExtractVarPartModel(assays(rse_gene_filt)$logcounts, formula, colData(rse_gene_filt))
+
+# Sort variables by median fraction of variance explained
+vp <- sortCols(varPart)
+
+p <- plotVarPart(vp)
+ggsave(
+    filename = here("plots/10_DEA/VarPartition.pdf"),
+    p, width = 40, height = 20, units = "cm"
+)
 
 ###############################################################################
 
@@ -85,4 +110,3 @@ options(width = 120)
 session_info()
 
 ###############################################################################
-
