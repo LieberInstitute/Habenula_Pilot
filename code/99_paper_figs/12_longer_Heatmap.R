afb13e0f6fda1ac3b0268059af36bb49b38f1fca
ajanakub <bukolaajanaku@gmail.com>
2023-06-05 10:59:16 -0400

    working on MAGMA analysis

--- code/99_paper_figs/12_longer_Heatmap.R
+++ code/99_paper_figs/12_longer_Heatmap.R
@@ -1,4 +1,4 @@
-## April 6, 2023 - Bukola Ajanaku
+## June 1, 2023 - Bukola Ajanaku
 # This is the exact code I used for the original progress report. Copy and pasted!
 # Simply changed plotting directory!!!
 # qrsh -l mem_free=30G,h_vmem=30G
---
+++ code/99_paper_figs/13_Wallace_MAGMA.R
@@ -0,0 +1,6 @@
+## June 5, 2023 - Bukola Ajanaku
+# Working on MAGMA trans-special analysis of our sce object against the 
+# Wallace et al. 2019 paper mouse data.
+# qrsh -l mem_free=50G,h_vmem=50G
+
+
