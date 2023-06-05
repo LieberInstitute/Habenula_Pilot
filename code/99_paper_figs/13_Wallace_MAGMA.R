203f139a863afe4bbceee7a7d2b17bf36eb30d4e
ajanakub <bukolaajanaku@gmail.com>
2023-06-05 11:08:17 -0400

    loading saved mouse data from Wallace et al

--- code/99_paper_figs/13_Wallace_MAGMA.R
+++ code/99_paper_figs/13_Wallace_MAGMA.R
@@ -3,4 +3,22 @@
 # Wallace et al. 2019 paper mouse data.
 # qrsh -l mem_free=50G,h_vmem=50G
 
+library("SingleCellExperiment")
+library("here")
+library("sessioninfo")
+library("jaffelab")
 
+# loading our final sce object
+load(file = here("processed-data", "99_paper_figs", "sce_objects", 
+                 "official_final_sce.RDATA"), verbose = TRUE)
+  # sce 
+
+# checking cluster data 
+table(sce$final_Annotations)
+
+# loading Wallace et al. clean data
+load(file = here("processed-data", "99_paper_figs", "MAGMA",
+                 "Wallace_mouse_data.rds"), verbose = TRUE)
+  # 
+
+# grabbed from Matt's code 
