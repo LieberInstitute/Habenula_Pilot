d59aba09f3839000b1ef7a8c11faa830238d5cc8
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-07 11:40:52 -0600

    load rse and rse filtered for analysis

--- code/10_DEA/04_DEA.R
+++ code/10_DEA/04_DEA.R
@@ -6,6 +6,38 @@ library(sessioninfo)
 
 
 
+###################### Load rse and rse filtered objects ######################
+
+load(
+    here(
+        "preprocessed_data",
+        "count_data_bukola",
+        "rse_gene_Roche_Habenula_qcAndAnnotated_n69.Rdata"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   rse_gene
+
+colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
+colData(rse_gene)$log10_library_size <- log10(colData(rse_gene)$library_size)
+colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x) {
+    length(x[which(x > 0)])
+})
+colData(rse_gene)$abs_ERCCsumLogErr <- abs(colData(rse_gene)$ERCCsumLogErr)
+
+load(
+    here(
+        "processed-data",
+        "10_DEA",
+        "rse_gene_filt.Rdata"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   rse_gene_filt
+
+###############################################################################
 
 ######################### Reproducibility information #########################
 
