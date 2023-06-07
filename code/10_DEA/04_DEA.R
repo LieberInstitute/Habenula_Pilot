512a6dbe4caab88d623fd00bfcefb6f5a89c63af
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-07 11:40:26 -0600

    started DEA script

---
+++ code/10_DEA/04_DEA.R
@@ -0,0 +1,19 @@
+library(here)
+library(SummarizedExperiment)
+library(edgeR)
+library(limma)
+library(sessioninfo)
+
+
+
+
+######################### Reproducibility information #########################
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+###############################################################################
