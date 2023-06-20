cfa0e1b70b930cf7a9ef3a79c633160552c1f94a
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-20 07:46:44 -0600

    Function to plot sample variables. Working on Issue #7

--- code/08_bulk_snpPC/02_explore_snpPCs.R
+++ code/08_bulk_snpPC/02_explore_snpPCs.R
@@ -47,6 +47,88 @@ class(snpPCs)
 ###############################################################################
 
 
+
+################################ Plotting PCs #################################
+
+colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
+colData(rse_gene)$log10_library_size <- log10(colData(rse_gene)$library_size)
+colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x) {
+    length(x[which(x > 0)])
+})
+colData(rse_gene)$abs_ERCCsumLogErr <- abs(colData(rse_gene)$ERCCsumLogErr)
+
+cols_rse <- colData(rse_gene)[order(colData(rse_gene)$BrNum), ]
+cols_rse <- cols_rse[cols_rse$BrNum != "Br6158",]
+snpPCs <- snpPCs[order(snpPCs$BrNum),]
+
+cols_rse <- cbind(snpPCs[,-1], cols_rse)
+cols_rse <- as.data.frame(cols_rse)
+
+sample_variables <- c("PrimaryDx", "Flowcell", "AgeDeath")
+
+pca_samplevars <- function(sample_var) {
+    pair_pc <- ggpairs(cols_rse,
+        columns = 1:4, aes_string(color = sample_var, alpha = 0.5),
+        upper = list(continuous = wrap("points", size = 2))
+    )
+
+    if (sample_var == "Flowcell") {
+        for (i in 1:4) {
+            for (j in 1:4) {
+                if (j != i) {
+                    plot <- ggplot(cols_rse, aes_string(x = paste0("snpPC", i), y = paste0("snpPC", j), color = sample_var, label = "BrNum")) +
+                        geom_point() +
+                        scale_color_manual(values = c("HVYTYBBXX" = "darkmagenta", "HW252BBXX" = "yellow3")) +
+                        geom_text() +
+                        theme_bw()
+                    pair_pc[i, j] <- plot
+                }
+            }
+        }
+    } else if (sample_var == "PrimaryDx") {
+        for (i in 1:4) {
+            for (j in 1:4) {
+                if (j != i) {
+                    plot <- ggplot(cols_rse, aes_string(x = paste0("snpPC", i), y = paste0("snpPC", j), color = sample_var, label = "BrNum")) +
+                        geom_point() +
+                        scale_color_manual(values = c("Schizo" = "darkgoldenrod3", "Control" = "turquoise3")) +
+                        geom_text() +
+                        theme_bw()
+                    pair_pc[i, j] <- plot
+                }
+            }
+        }
+    } else if (sample_var == "AgeDeath") {
+        for (i in 1:4) {
+            for (j in 1:4) {
+                if (j != i) {
+                    plot <- ggplot(cols_rse, aes_string(x = paste0("snpPC", i), y = paste0("snpPC", j), color = sample_var, label = "BrNum")) +
+                        geom_point() +
+                        scale_color_gradient(low = "#F19E93", high = "#00433F") +
+                        geom_text() +
+                        theme_bw()
+                    pair_pc[i, j] <- plot
+                }
+            }
+        }
+    }
+
+    ggsave(
+        paste(output_path, "/PCA_", sample_var, ".pdf", sep = ""),
+        pair_pc,
+        width = 30,
+        height = 34,
+        units = "cm"
+    )
+}
+
+lapply(sample_variables, pca_samplevars)
+
+
+###############################################################################
+
+
+
 ######################### Reproducibility information #########################
 
 ## Reproducibility information
