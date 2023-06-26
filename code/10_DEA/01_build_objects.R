31732e6842a514968b6c0d5966667692d7104a80
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-26 14:34:07 -0400

    Add some qc metrics

--- code/10_DEA/01_build_objects.R
+++ code/10_DEA/01_build_objects.R
@@ -52,6 +52,18 @@ snpPCs <- read.table(
 ###############################################################################
 
 
+
+########################## Add qc metrics to colData ##########################
+
+colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
+colData(rse_gene)$log10_library_size <- log10(colData(rse_gene)$library_size)
+colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x) {
+    length(x[which(x > 0)])
+})
+colData(rse_gene)$abs_ERCCsumLogErr <- abs(colData(rse_gene)$ERCCsumLogErr)
+
+###############################################################################
+
 ######################### Reproducibility information #########################
 
 ## Reproducibility information
