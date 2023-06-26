2274e9855cc8c756d1c039c4a889281b9849a5da
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-06-26 14:33:04 -0400

    Load deconvolution and SNP PCs data. qSVa results are not available yet

--- code/10_DEA/01_build_objects.R
+++ code/10_DEA/01_build_objects.R
@@ -20,6 +20,38 @@ load(
 ###############################################################################
 
 
+
+################# Load deconvolution, SNP PCs and qSVa results ################
+
+## Load deconvolution results
+load(
+    here(
+        "processed-data",
+        "06_deconvolution",
+        "run_Bisque",
+        "est_prop_split_Hb_annotations.RDATA"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   est_prop
+
+
+## Load SNP PCs
+snpPCs <- read.table(
+    here(
+        "processed-data",
+        "08_bulk_snpPC",
+        "habenula_genotypes_filt.snpPCs.tab"
+    ),
+    header = TRUE
+)
+
+## Load qSVa
+
+###############################################################################
+
+
 ######################### Reproducibility information #########################
 
 ## Reproducibility information
