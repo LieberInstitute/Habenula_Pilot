4e936b75cc2322fb2263dbe76472a500e42ba0d9
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-11 07:32:43 -0600

    Copy boxplots and corr plots from original DataExploration script

--- code/10_DEA/02_DataExploration_v2.R
+++ code/10_DEA/02_DataExploration_v2.R
@@ -38,6 +38,114 @@ dim(rse_gene)
 
 
 
+################### Boxpots: sample_variables - qc_metrics ####################
+
+## NOTE: This section is done with unfiltered and not normalized counts
+
+colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
+colData(rse_gene)$log10_library_size <- log10(colData(rse_gene)$library_size)
+colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x) {
+    length(x[which(x > 0)])
+})
+colData(rse_gene)$abs_ERCCsumLogErr <- abs(colData(rse_gene)$ERCCsumLogErr)
+
+qc_metrics <- c("mitoRate", "rRNA_rate", "overallMapRate", "totalAssignedGene", "concordMapRate", "log10_library_size", "detected_num_genes", "RIN", "abs_ERCCsumLogErr")
+sample_variables <- c("PrimaryDx", "Flowcell")
+
+
+## Function to create boxplots of QC metrics for groups of samples
+QC_boxplots <- function(qc_metric, sample_var) {
+    if (sample_var == "PrimaryDx") {
+        colors <- c("HVYTYBBXX" = "darkmagenta", "HW252BBXX" = "yellow3")
+        x_label <- "PrimaryDx"
+        sample_var_v2 <- "Flowcell"
+    } else if (sample_var == "Flowcell") {
+        colors <- c("Schizo" = "darkgoldenrod3", "Control" = "turquoise3")
+        x_label <- "Flowcell"
+        sample_var_v2 <- "PrimaryDx"
+    }
+
+    y_label <- str_replace_all(qc_metric, c("_" = " "))
+
+    data <- data.frame(colData(rse_gene))
+    plot <- ggplot(data = data, mapping = aes(x = !!rlang::sym(sample_var), y = !!rlang::sym(qc_metric), color = !!rlang::sym(sample_var_v2))) +
+        theme_bw() +
+        geom_violin(alpha = 0, size = 0.4, color = "black", width = 0.7) +
+        geom_jitter(width = 0.1, alpha = 0.7, size = 2) +
+        geom_boxplot(alpha = 0, size = 0.4, width = 0.1, color = "black") +
+        scale_color_manual(values = colors) +
+        labs(y = y_label, x = x_label) +
+        theme(
+            axis.title = element_text(size = (9)),
+            axis.text = element_text(size = (8))
+        )
+
+    return(plot)
+}
+
+for (sample_var in sample_variables) {
+    i <- 1
+    plots <- list()
+    for (qc_metric in qc_metrics) {
+        plots[[i]] <- QC_boxplots(qc_metric, sample_var)
+        i <- i + 1
+    }
+    plot_grid(plotlist = plots, nrow = 3)
+
+    ggsave(
+        paste(output_path, "/QC_boxplots_", sample_var, ".pdf", sep = ""),
+        width = 35,
+        height = 30,
+        units = "cm"
+    )
+}
+
+###############################################################################
+
+
+
+################## Correlation plots: AgeDeath - qc_metrics ###################
+
+## NOTE: This section is done with unfiltered and not normalized counts
+
+rse_gene_df <- data.frame(colData(rse_gene))
+
+## Calculate correlations with Pearson
+corrs_age <- sapply(
+    qc_metrics,
+    function(x) {
+        round(cor(rse_gene_df$AgeDeath, rse_gene_df[, x], method = c("pearson")), 3)
+    }
+)
+
+## Function to plot
+plot_correlations <- function(qc_metric) {
+    ggplot(rse_gene_df, aes_string(x = "AgeDeath", y = qc_metric)) +
+        geom_point(aes_string(colour = "PrimaryDx")) +
+        scale_color_manual(values = c("Schizo" = "darkgoldenrod3", "Control" = "turquoise3")) +
+        stat_smooth(geom = "line", alpha = 0.7, size = 1.1, span = 0.1, method = lm, show.legend = FALSE) +
+        theme_bw() +
+        xlab("Age (when death)") +
+        ylab(str_replace_all(qc_metric, pattern = "_", replacement = " ")) +
+        annotate("text",
+            x = max(rse_gene_df$AgeDeath) - max(rse_gene_df$AgeDeath) / 10,
+            y = max(rse_gene_df[, qc_metric]) - (max(rse_gene_df[, qc_metric]) - min(rse_gene_df[, qc_metric])) / 10,
+            label = paste0("r = ", corrs_age[qc_metric])
+        )
+}
+
+corr_plots <- lapply(qc_metrics, plot_correlations)
+ggsave(
+    paste(output_path, "/Corr_AgeDeath_vs_QCmetrics.pdf", sep = ""),
+    plot_grid(plotlist = corr_plots, nrow = 3),
+    width = 40,
+    height = 30,
+    units = "cm"
+)
+
+###############################################################################
+
+
 
 ################## PCA and heatmap with colData() variables ###################
 
