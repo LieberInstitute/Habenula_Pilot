163f340b7ef9c223c282a8994b750eb1fd095044
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-11 14:36:50 -0600

    Run DEA and plots with 6 differnt models, described in the script

--- code/10_DEA/04_DEA.R
+++ code/10_DEA/04_DEA.R
@@ -39,7 +39,7 @@ load(
 
 
 
-####################E### Functions for DEA and plotting #######################
+#################### E### Functions for DEA and plotting #######################
 
 norm_factors <- calcNormFactors(rse_gene_raw, method = "TMM")
 samples_factors <- data.frame(
@@ -134,15 +134,146 @@ plot_volc <- function(top_genes, FDR_cut, model_name) {
 
 ##################################### DEA #####################################
 
-## Model without totalAssignedGene, no qSVs and no tot.Tha
-formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + abs_ERCCsumLogErr + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 + tot.Hb
-res_formula_1 <- DE_analysis(rse_gene = rse_gene, formula = formula, coef = "PrimaryDxSchizo", model_name = "qc-snpPCs-Hb")
-plot_volc(res_formula_1, FDR_cut = 5e-02, model_name = "qc-snpPCs-Hb")
+## Model qc-snpPCs-Hb
+## WITHOUT - totalAssignedGene, qSVs and tot.Thal
+formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + abs_ERCCsumLogErr +
+    snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+    tot.Hb
 
-## Model without totalAssignedGene and no tot.Tha, but with qSVs
-formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + abs_ERCCsumLogErr + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 + qSV1 + qSV2 + qSV3 + qSV4 + qSV5 + tot.Hb
-res_formula_1 <- DE_analysis(rse_gene = rse_gene, formula = formula, coef = "PrimaryDxSchizo", model_name = "qc-snpPCs-Hb-qSVs")
-plot_volc(res_formula_1, FDR_cut = 5e-02, model_name = "qc-snpPCs-Hb-qSVs")
+res_formula <- DE_analysis(
+    rse_gene = rse_gene,
+    formula = formula,
+    coef = "PrimaryDxSchizo",
+    model_name = "qc-snpPCs-Hb"
+)
+
+plot_volc(res_formula, FDR_cut = 5e-02, model_name = "qc-snpPCs-Hb")
+
+summary(res_formula$adj.P.Val)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.1267  0.1267  0.1267  0.1568  0.1408  0.9996
+
+
+## Model qc-snpPCs-Hb-qSVs
+## WITH - qSVs
+## WITHOUT - totalAssignedGene and tot.Thal
+formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + abs_ERCCsumLogErr +
+    snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+    qSV1 + qSV2 + qSV3 + qSV4 + qSV5 +
+    tot.Hb
+
+res_formula <- DE_analysis(
+    rse_gene = rse_gene,
+    formula = formula,
+    coef = "PrimaryDxSchizo",
+    model_name = "qc-snpPCs-Hb-qSVs"
+)
+
+plot_volc(
+    res_formula,
+    FDR_cut = 5e-02,
+    model_name = "qc-snpPCs-Hb-qSVs"
+)
+
+summary(res_formula$adj.P.Val)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.3325  0.3325  0.3325  0.3704  0.3417  1.0000
+
+
+## Model qc-totAssGene-snpPCs-Hb
+## WITH - totalAssignedGene
+## WITHOUT - qSVs and tot.Thal
+formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + totalAssignedGene + abs_ERCCsumLogErr +
+    snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+    tot.Hb
+
+res_formula <- DE_analysis(
+    rse_gene = rse_gene,
+    formula = formula, coef = "PrimaryDxSchizo",
+    model_name = "qc-totAssGene-snpPCs-Hb"
+)
+
+plot_volc(res_formula,
+    FDR_cut = 5e-02,
+    model_name = "qc-totAssGene-snpPCs-Hb"
+)
+
+summary(res_formula$adj.P.Val)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.2338  0.2338  0.2338  0.2725  0.2541  0.9992
+
+
+## Model qc-totAssGene-snpPCs-Hb-qSVs
+## WITH - totalAssignedGene and qSVs
+## WITHOUT - tot.Thal
+formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + totalAssignedGene + abs_ERCCsumLogErr +
+    snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+    qSV1 + qSV2 + qSV3 + qSV4 + qSV5 +
+    tot.Hb
+
+res_formula <- DE_analysis(
+    rse_gene = rse_gene,
+    formula = formula, coef = "PrimaryDxSchizo",
+    model_name = "qc-totAssGene-snpPCs-Hb-qSVs"
+)
+
+plot_volc(res_formula,
+    FDR_cut = 5e-02,
+    model_name = "qc-totAssGene-snpPCs-Hb-qSVs"
+)
+
+summary(res_formula$adj.P.Val)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.4664  0.4664  0.4664  0.5008  0.4720  0.9993
+
+
+## Model qc-totAssGene-snpPCs-Hb-Thal
+## WITH - totalAssignedGene and tot.Thal
+## WITHOUT - qSVs
+formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + totalAssignedGene + abs_ERCCsumLogErr +
+    snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+    tot.Hb + tot.Thal
+
+res_formula <- DE_analysis(
+    rse_gene = rse_gene,
+    formula = formula,
+    coef = "PrimaryDxSchizo",
+    model_name = "qc-totAssGene-snpPCs-Hb-Thal"
+)
+
+plot_volc(res_formula,
+    FDR_cut = 5e-02,
+    model_name = "qc-totAssGene-snpPCs-Hb-Thal"
+)
+
+summary(res_formula$adj.P.Val)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.3990  0.3990  0.3990  0.4345  0.4090  0.9997
+
+
+## Model qc-totAssGene-snpPCs-Hb-Thal-qSVs
+## WITH - totalAssignedGene, tot.Thal and qSVs
+## WITHOUT -
+formula <- ~ PrimaryDx + AgeDeath + Flowcell + mitoRate + rRNA_rate + RIN + totalAssignedGene + abs_ERCCsumLogErr +
+    snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+    qSV1 + qSV2 + qSV3 + qSV4 + qSV5 +
+    tot.Hb + tot.Thal
+
+res_formula <- DE_analysis(
+    rse_gene = rse_gene,
+    formula = formula,
+    coef = "PrimaryDxSchizo",
+    model_name = "qc-totAssGene-snpPCs-Hb-Thal-qSVs"
+)
+
+plot_volc(res_formula,
+    FDR_cut = 5e-02,
+    model_name = "qc-totAssGene-snpPCs-Hb-Thal-qSVs"
+)
+
+summary(res_formula$adj.P.Val)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.4664  0.4664  0.4664  0.5008  0.4720  0.9993
 
 ###############################################################################
 
