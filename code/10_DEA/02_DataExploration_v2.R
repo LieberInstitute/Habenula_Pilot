889796c0fd3de27abfc5c3ef7ef3c73498c5061d
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-11 07:32:05 -0600

    Plot correlation hetmaps with and without qSVs

--- code/10_DEA/02_DataExploration_v2.R
+++ code/10_DEA/02_DataExploration_v2.R
@@ -45,6 +45,16 @@ dim(rse_gene)
 
 ## PCA
 pca_df <- pca(assays(rse_gene)$logcounts, metadata = colData(rse_gene))
+
+## Plot correlation between PCs and variables (with and without qSVs)
+pdf(paste(output_path, "/", "Corr_PCA-Vars-qSVs.pdf", sep = ""), width = 10, height = 10)
+eigencorplot(pca_df, metavars = c("PrimaryDx", "AgeDeath", "Flowcell", "mitoRate", "rRNA_rate", "totalAssignedGene", "RIN", "abs_ERCCsumLogErr", "snpPC1", "snpPC2", "snpPC3", "snpPC4", "snpPC5", "tot.Hb", "tot.Thal", "qSV1", "qSV2", "qSV3", "qSV4", "qSV5"))
+dev.off()
+
+pdf(paste(output_path, "/", "Corr_PCA-Vars-noqSVs.pdf", sep = ""), width = 10, height = 10)
+eigencorplot(pca_df, metavars = c("PrimaryDx", "AgeDeath", "Flowcell", "mitoRate", "rRNA_rate", "totalAssignedGene", "RIN", "abs_ERCCsumLogErr", "snpPC1", "snpPC2", "snpPC3", "snpPC4", "snpPC5", "tot.Hb", "tot.Thal"))
+dev.off()
+
 ###############################################################################
 
 
