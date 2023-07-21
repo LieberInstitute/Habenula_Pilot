8c99a0e0c1316ce40420e769c36e512b5384dd30
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-21 15:42:54 -0600

    changed which files I am loading

--- code/10_DEA/05_GOenrichment.R
+++ code/10_DEA/05_GOenrichment.R
@@ -13,32 +13,29 @@ out_data <- here("processed-data", "10_DEA", "05_GOenrichment")
 
 ####################### Load tsv with all genes from DEA ######################
 
-## For now I'm just gonna load these two models: qc-snpPCs-Hb and
-## qc-totAssGene-snpPCs-Hb beacuse they had the best results
-
-DE_qc_snpPCs_Hb <- fread(
+DEG_all <- fread(
     here(
         "processed-data",
         "10_DEA",
         "04_DEA",
-        "DEA_AllGenes_qc-snpPCs-Hb.tsv"
+        "DEA_AllGenes_qc-totAGene-qSVs-Hb-Thal.tsv"
     ),
     sep = "\t",
     data.table = FALSE,
     stringsAsFactors = FALSE
 )
 
-# DE_qc_totAssGene_snpPCs_Hb <- fread(
-#     here(
-#         "processed-data",
-#         "10_DEA",
-#         "04_DEA",
-#         "DEA_AllGenes_qc-totAssGene-snpPCs-Hb.tsv"
-#     ),
-#     sep = "\t",
-#     data.table = FALSE,
-#     stringsAsFactors = FALSE
-# )
+DEG_sig <- fread(
+    here(
+        "processed-data",
+        "10_DEA",
+        "04_DEA",
+        "DEA_SigGenes_FDR1_qc-totAGene-qSVs-Hb-Thal.tsv"
+    ),
+    sep = "\t",
+    data.table = FALSE,
+    stringsAsFactors = FALSE
+)
 
 ###############################################################################
 
@@ -46,12 +43,10 @@ DE_qc_snpPCs_Hb <- fread(
 
 ######################### Prepare data fo GO analysis #########################
 
-sigGene <- DE_qc_snpPCs_Hb %>% filter(adj.P.Val < 0.15 & abs(logFC) > 1)
-
-sigGene <- split(sigGene$EntrezID, sign(sigGene$logFC))
+sigGene <- split(DEG_sig$EntrezID, sign(DEG_sig$logFC))
 sigGene <- lapply(sigGene, function(x) x[!is.na(x)])
 
-geneUniverse <- as.character(DE_qc_snpPCs_Hb$EntrezID)
+geneUniverse <- as.character(DEG_all$EntrezID)
 geneUniverse <- geneUniverse[!is.na(geneUniverse)]
 
 ###############################################################################
@@ -66,8 +61,8 @@ go <- compareCluster(sigGene,
     OrgDb = org.Hs.eg.db,
     ont = "ALL",
     pAdjustMethod = "BH",
-    pvalueCutoff = 0.1,
-    qvalueCutoff = 0.05,
+    pvalueCutoff = 0.15,
+    #qvalueCutoff = 0.05,
     readable = TRUE
 )
 
@@ -75,7 +70,7 @@ kegg <- compareCluster(sigGene,
     fun = "enrichKEGG",
     universe = geneUniverse,
     organism = "human",
-    pvalueCutoff = 0.1,
+    pvalueCutoff = 0.15,
     qvalueCutoff = 0.05
 )
 
