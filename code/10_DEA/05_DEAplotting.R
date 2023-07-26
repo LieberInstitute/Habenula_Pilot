cc746a6141f3902e52b9cb1b8e78cdf8d32bf40d
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-26 07:21:21 -0600

    load all the DEA result tsvs

--- code/10_DEA/05_DEAplotting.R
+++ code/10_DEA/05_DEAplotting.R
@@ -9,6 +9,22 @@ library("sessioninfo")
 out_plot <- here("plots", "10_DEA", "04_DEA")
 
 
+
+################## Load significant results from DEA objects ##################
+
+dea_res <- list.files(
+    here(
+        "processed-data", "10_DEA", "04_DEA"
+    ),
+    pattern = "DEA_All*",
+    full.names = TRUE
+)
+
+dea_res <- lapply(dea_res, fread, data.table = FALSE)
+
+
+###############################################################################
+
 ######################### Reproducibility information #########################
 
 ## Reproducibility information
