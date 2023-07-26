17956e379dd7e497158ce7205a62d3511e181efc
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-26 13:45:12 -0600

    Add needed files

--- code/12_GSEA/02_GSE-plot.R
+++ code/12_GSEA/02_GSE-plot.R
@@ -10,6 +10,38 @@ out_plot <- here("plots", "12_GSEA")
 
 
 
+############## Load objects for gene_set_enrichment_plot_complex ##############
+
+## Load gene_set_enrichment_1vsAll_result_tables.rda
+load(
+    here(
+        "processed-data",
+        "12_GSEA",
+        "gene_set_enrichment_1vsAll_result_tables.rda"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   enrichTab_FDR05
+#   enrichTab_FDR1
+#   gene_list
+
+## Load sce_modeling_results.Rdata
+load(
+    here(
+        "processed-data",
+        "05_explore_sce",
+        "04_sce_1vALL_modeling",
+        "sce_modeling_results.Rdata"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   sce_modeling_results
+
+###############################################################################
+
+
 ######################### Reproducibility information #########################
 
 print("Reproducibility information:")
