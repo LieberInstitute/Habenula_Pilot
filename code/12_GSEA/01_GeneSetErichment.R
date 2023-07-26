f5138107a0968aabc2fbde3f47aacd69af365ee1
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-26 08:47:42 -0600

    started script for gene set enrichment andalysis

---
+++ code/12_GSEA/01_GeneSetErichment.R
@@ -0,0 +1,18 @@
+library("here")
+library("data.table")
+library("dplyr")
+library("spatialLIBD")
+library("sessioninfo")
+
+
+######################### Reproducibility information #########################
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+###############################################################################
+
