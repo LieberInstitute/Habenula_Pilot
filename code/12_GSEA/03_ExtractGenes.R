03ea608c1aae304e2b166eca73466232745ab9a7
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-07-29 09:13:02 -0600

    load all the needed files

--- code/12_GSEA/03_ExtractGenes.R
+++ code/12_GSEA/03_ExtractGenes.R
@@ -6,6 +6,66 @@ library("sessioninfo")
 out_data <- here("processed-data", "12_GSEA")
 
 
+
+###################### Load result tables and sce objects #####################
+
+## Load gene_set_enrichment_1vsAll_result_tables.rda
+load(
+    here(
+        "processed-data",
+        "12_GSEA",
+        "gene_set_enrichment_1vsAll_result_tables.rda"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   enrichTab_FDR05
+#   enrichTab_FDR1
+#   gene_list
+
+## Load sce_modeling_final_Annotations.Rdata
+load(
+    here(
+        "processed-data",
+        "05_explore_sce",
+        "04_sce_1vALL_modeling",
+        "sce_modeling_final_Annotations.Rdata"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   sce_modeling_final_Annotations
+
+
+## Load gene_set_enrichment_1vsAll_result_tables.rda
+load(
+    here(
+        "processed-data",
+        "12_GSEA",
+        "gene_set_enrichment_1vsAll_broad_result_tables.rda"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   enrichTab_br_FDR05
+#   enrichTab_br_FDR1
+#   gene_list
+
+## Load sce_modeling_broad_Annotations.Rdata
+load(
+    here(
+        "processed-data",
+        "05_explore_sce",
+        "04_sce_1vALL_modeling",
+        "sce_modeling_broad_Annotations.Rdata"
+    ),
+    verbose = TRUE
+)
+# Loading objects:
+#   sce_modeling_broad_Annotations
+
+###############################################################################
+
 ######################### Reproducibility information #########################
 
 ## Reproducibility information
