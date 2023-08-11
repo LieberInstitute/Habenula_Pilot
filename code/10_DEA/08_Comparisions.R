e29d3327b459beabf95bf7f23d9f34d2970b7181
Renee Garcia-Flores <renee.garciaflores@gmail.com>
2023-08-11 08:03:49 -0600

    correct typo

--- code/10_DEA/08_Comparisions.R
+++ code/10_DEA/08_Comparisions.R
@@ -4,9 +4,9 @@ library("VennDiagram")
 library("dplyr")
 library("sessioninfo")
 
-out_plot <- here("plots", "10_DEA", "08_Comparisions")
+out_plot <- here("plots", "10_DEA", "08_Comparisons")
 if (!dir.exists(out_plot)) dir.create(out_plot)
-out_data <- here("processed-data", "10_DEA", "08_Comparisions")
+out_data <- here("processed-data", "10_DEA", "08_Comparisons")
 if (!dir.exists(out_data)) dir.create(out_data)
 
 
