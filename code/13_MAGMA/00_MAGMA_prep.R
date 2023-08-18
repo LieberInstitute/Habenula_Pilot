9d8e23d25efbdb323fc412fb45f9071b5c2e0735
lahuuki <lahuuki@gmail.com>
2023-08-18 16:42:02 -0400

    Start MAGMA step 2, ERROR - processing individuals: remaining sample is too small

--- code/13_MAGMA/00_MAGMA_prep.R
+++ code/13_MAGMA/00_MAGMA_prep.R
@@ -8,10 +8,12 @@ library("sessioninfo")
 
 #### SNP loc ####
 ## use bim file
-# use a .bim file from binary PLINK data, this can be provided to MAGMA as a SNP location file without modification.
+# "use a .bim file from binary PLINK data, this can be provided to MAGMA as a SNP location file without modification."
+# ^ So that was a lie * this bim file didn't work (I think because of 'chr' prefix)
+
 bim_fn <- here("processed-data","08_bulk_snpPC", "habenula_genotypes.bim")
-# bim <- read.table(bim_fn)
-# head(bim)
+bim <- read.table(bim_fn)
+head(bim)
 # # V1                 V2 V3    V4 V5    V6
 # # 1 chr1 chr1:11171:CCTTG:C  0 11171  C CCTTG
 # # 2 chr1     chr1:23308:G:C  0 23308  C     G
@@ -23,23 +25,49 @@ bim_fn <- here("processed-data","08_bulk_snpPC", "habenula_genotypes.bim")
 # nrow(bim)
 # [1] 12393872
 
+snploc <- bim |>
+  select(SNP = V2, CHR = V1, BP = V4) |>
+  mutate(CHR = gsub("chr","", CHR))
+
+head(snploc)
+
+snploc |> count(CHR)
+
+snploc_fn <- here("processed-data", "13_MAGMA", "habenula_genotypes.snploc")
+
+write.table(snploc,
+            file = snploc_fn,
+            sep = "\t", col.names = T, row.names = F, quote = F
+)
+
 #### Step 1 annotation  ####
 
 ## use gene loc build 38 from MAGAM website
 geneloc_fn <- here("processed-data", "13_MAGMA", "NCBI38","NCBI38.gene.loc")
 
-output_dir <- here("processed-data", "13_MAGMA", "01_MAGMA_annotate")
+output_dir <- here("processed-data", "13_MAGMA", "MAGMA_output")
+if(!dir.exists(output_dir)) dir.create(output_dir)
+
+ANNOT_PREFIX <- here(output_dir, "Habenula_MAGMA")
 
 sgejobs::job_single('01_MAGMA_annotate', create_shell = TRUE, memory = '25G', 
                     command = paste0("magma --annotate --snp-loc ", bim_fn,
-                                     " --gene-loc ", geneloc_fn,
-                                     " --out ", output_dir))
+                                     " --gene-loc ", snploc_fn,
+                                     " --out ", ANNOT_PREFIX))
 
-#### Step 2 ####
+#### Step 2 Gene analysis ####
+GENE_PREFIX <- here(output_dir, "Habenula_MAGMA_gene") 
+
+sgejobs::job_single('02_MAGMA_gene', create_shell = TRUE, memory = '25G', 
+                    command = paste0("magma --bfile ", "/dcs04/lieber/lcolladotor/with10x_LIBD001/HumanPilot/Analysis/Layer_Guesses/MAGMA/g1000_eur", 
+                                     "--gene-annot ", ANNOT_PREFIX,".genes.annot",
+                                     "--out ", GENE_PREFIX))
 
 
 #### Step 3 Gene-set Analysis ####
+GS_PREFIX
 
+"magma --bfile [REFDATA] --pval [PVAL_FILE] N=[N] --gene-annot [ANNOT_PREFIX].genes.annot \ --out [GENE_PREFIX]" 
 ## create SET_FILE with each row corresponding to a gene set: name of the gene set followed by the gene IDs, separated by whitespace).
 
 
---
+++ code/13_MAGMA/02_MAGMA_gene.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=25G,h_vmem=25G,h_fsize=100G
+#$ -N MAGMA_gene
+#$ -o logs/02_MAGMA_gene.txt
+#$ -e logs/02_MAGMA_gene.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load magma/1.10
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+magma --bfile /dcs04/lieber/lcolladotor/with10x_LIBD001/HumanPilot/Analysis/Layer_Guesses/MAGMA/g1000_eur\
+		--gene-annot /dcs04/lieber/lcolladotor/pilotHb_LIBD001/Roche_Habenula/processed-data/13_MAGMA/MAGMA_output/Habenula_MAGMA.genes.annot\
+		--out /dcs04/lieber/lcolladotor/pilotHb_LIBD001/Roche_Habenula/processed-data/13_MAGMA/MAGMA_output/Habenula_MAGMA_gene
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
