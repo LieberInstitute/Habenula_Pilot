851c9de68a77e2779986514fa1af6a072521c037
lahuuki <lahuuki@gmail.com>
2023-09-13 12:23:22 -0400

    Annotate SCZ snps, SLURM sh file

---
+++ code/13_MAGMA/01_MAGMA_annotate_SCZ.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH --output=logs/01_MAGMA_annotate_SCZ.txt
+#SBATCH --error=logs/01_MAGMA_annotate_SCZ.txt
+#SBATCH --partition=shared
+#SBATCH --job-name=MAGMA_annotate_SCZ
+#SBATCH --mem=25GB
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## load modules
+module load magma/1.10
+# list modules
+module list
+
+magma --annotate\
+	--snp-loc ../../processed-data/13_MAGMA/SCZ/PGC3_SCZ_wave3.european.autosome.public.v3.snploc\
+	--gene-loc ../../processed-data/13_MAGMA/NCBI37.3/NCBI37.3.gene.loc\
+	--out ../../processed-data/13_MAGMA/SCZ
+
+echo "**** Job ends ****"
+date
