53fe2a6fbe121c5e9550d74069cd35c6a4f5d5ae
lahuuki <lahuuki@gmail.com>
2023-09-13 14:47:32 -0400

    Run enrichment on broad cell type markers

---
+++ code/13_MAGMA/03_MAGMA_gene-set_SCZ.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#SBATCH --output=logs/03_MAGMA_gene-set_SCZ.txt
+#SBATCH --error=logs/03_MAGMA_gene-set_SCZ.txt
+#SBATCH --partition=shared
+#SBATCH --job-name=MAGMA_gene-set_SCZ
+#SBATCH --mem=25GB
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## load modules
+module load magma/1.10
+# list modules
+module list
+
+magma --gene-results ../../processed-data/13_MAGMA/SCZ/PGC3_SCZ_wave3.european.autosome.public.v3.genes.raw\
+	--set-annot ../../processed-data/13_MAGMA/gene_sets/markerSets_broad_FDR05.txt gene-col=Gene set-col=Set\
+	--out ../../processed-data/13_MAGMA/SCZ_broad
+
+echo "**** Job ends ****"
+date
