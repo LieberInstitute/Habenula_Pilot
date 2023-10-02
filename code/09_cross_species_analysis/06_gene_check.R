09ba050f26229a48bd653afbce1fb8053bacebc5
lahuuki <lahuuki@gmail.com>
2023-10-02 15:20:20 -0400

    Add CHRNB4

--- code/09_cross_species_analysis/06_gene_check.R
+++ code/09_cross_species_analysis/06_gene_check.R
@@ -85,12 +85,13 @@ search_tb |>
 # <chr>               <int> <chr>   <chr>   <chr>      <dbl> <dbl>
 #   1 Mhb.1            44789930 CCK     Cck     MHb5      0.113   5.37
 # 2 Mhb.2            44790260 CHAT    Chat    MHb6      0.0585  4.37
-# 3 Mhb.3            44791337 EBF3    Ebf3    MHb6      0.263   2.06
-# 4 LHb              44795328 HTR4    Htr4    MHb2      0.981   1.98
-# 5 Lhb.3            44800479 ONECUT2 Onecut2 LHb2      0.171   2.67
-# 6 LHb.1            44784746 SEMA3D  Sema3d  LHb4      0.417   2.12
-# 7 Mhb.1            44791974 TAC3    Tac2    MHb6      0.112   3.55
-# 8 LHb.4            44789237 TLE2    Tle2    LHb2      0.120   2.46
+# 3 Mhb              44791996 CHRNB4  Chrnb4  MHb6      0.102   3.44
+# 4 Mhb.3            44791337 EBF3    Ebf3    MHb6      0.263   2.06
+# 5 LHb              44795328 HTR4    Htr4    MHb2      0.981   1.98
+# 6 Lhb.3            44800479 ONECUT2 Onecut2 LHb2      0.171   2.67
+# 7 LHb.1            44784746 SEMA3D  Sema3d  LHb4      0.417   2.12
+# 8 Mhb.1            44791974 TAC3    Tac2    MHb6      0.112   3.55
+# 9 LHb.4            44789237 TLE2    Tle2    LHb2      0.120   2.46
 
 search_tb |> 
   left_join(enrich_long_mm_all, relationship = "many-to-many") |>
@@ -104,13 +105,14 @@ search_tb |>
 #   1 Mhb.3            44787249 BHLHE22 Bhlhe22 OPC3      0.123        2.88
 # 2 Mhb.1            44789930 CCK     Cck     Neuron7   0.493        4.16
 # 3 Mhb.2            44790260 CHAT    Chat    Neuron7   0.194        4.41
-# 4 Mhb.3            44791337 EBF3    Ebf3    Microglia 0.227        2.09
-# 5 LHb.6            44794749 ESRP1   Esrp1   OPC1      0.000000233  5.69
-# 6 LHb              44795328 HTR4    Htr4    Neuron7   0.496        2.39
-# 7 Lhb.3            44800479 ONECUT2 Onecut2 Oligo3    0.171        4.39
-# 8 LHb.1            44784746 SEMA3D  Sema3d  OPC3      0.0759       5.13
-# 9 Mhb.1            44791974 TAC3    Tac2    Neuron7   0.278        5.26
-# 10 LHb.4            44789237 TLE2    Tle2    OPC2      0.489        2.95
+# 4 Mhb              44791996 CHRNB4  Chrnb4  Neuron7   0.379        4.69
+# 5 Mhb.3            44791337 EBF3    Ebf3    Microglia 0.227        2.09
+# 6 LHb.6            44794749 ESRP1   Esrp1   OPC1      0.000000233  5.69
+# 7 LHb              44795328 HTR4    Htr4    Neuron7   0.496        2.39
+# 8 Lhb.3            44800479 ONECUT2 Onecut2 Oligo3    0.171        4.39
+# 9 LHb.1            44784746 SEMA3D  Sema3d  OPC3      0.0759       5.13
+# 10 Mhb.1            44791974 TAC3    Tac2    Neuron7   0.278        5.26
+# 11 LHb.4            44789237 TLE2    Tle2    OPC2      0.489        2.95
 
 enrich_long_hs_all <- hsap_modeling_results$all$enrichment |>
   select(starts_with("fdr"), ensembl, gene)|>
@@ -129,13 +131,14 @@ search_tb |>
 
 # cell_type_search  ensembl hs_gene gene    cell_type           fdr logFC
 # <chr>               <int> <chr>   <chr>   <chr>             <dbl> <dbl>
-# 1 Mhb.3            44787249 BHLHE22 BHLHE22 MHb.3      0.000171      6.59
+#   1 Mhb.3            44787249 BHLHE22 BHLHE22 MHb.3      0.000171      6.59
 # 2 Mhb.1            44789930 CCK     CCK     Excit.Thal 0.108         3.60
 # 3 Mhb.2            44790260 CHAT    CHAT    MHb.2      0.000214      5.38
-# 4 Mhb.3            44791337 EBF3    EBF3    MHb.3      0.171         7.19
-# 5 LHb.6            44794749 ESRP1   ESRP1   LHb.6      0.368         4.48
-# 6 LHb              44795328 HTR4    HTR4    LHb.7      0.801         2.29
-# 7 Lhb.3            44800479 ONECUT2 ONECUT2 LHb.1      0.178         4.64
-# 8 LHb.1            44784746 SEMA3D  SEMA3D  Astrocyte  0.203         3.49
-# 9 Mhb.1            44791974 TAC3    TAC3    MHb.1      0.0000000200  7.46
-# 10 LHb.4            44789237 TLE2    TLE2    Inhib.Thal 0.112         3.10
+# 4 Mhb              44791996 CHRNB4  CHRNB4  MHb.2      0.0978        3.61
+# 5 Mhb.3            44791337 EBF3    EBF3    MHb.3      0.171         7.19
+# 6 LHb.6            44794749 ESRP1   ESRP1   LHb.6      0.368         4.48
+# 7 LHb              44795328 HTR4    HTR4    LHb.7      0.801         2.29
+# 8 Lhb.3            44800479 ONECUT2 ONECUT2 LHb.1      0.178         4.64
+# 9 LHb.1            44784746 SEMA3D  SEMA3D  Astrocyte  0.203         3.49
+# 10 Mhb.1            44791974 TAC3    TAC3    MHb.1      0.0000000200  7.46
+# 11 LHb.4            44789237 TLE2    TLE2    Inhib.Thal 0.112         3.10
