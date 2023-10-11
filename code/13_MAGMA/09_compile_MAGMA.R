fc281c654949ab68f4ac460675be1c3d1fc504e0
lahuuki <lahuuki@gmail.com>
2023-10-11 15:59:15 -0400

    Add MAGAM p-val heatmap

--- code/13_MAGMA/09_compile_MAGMA.R
+++ code/13_MAGMA/09_compile_MAGMA.R
@@ -14,22 +14,45 @@ magama_out <- map_dfr(magma_out_fn, ~read.table(.x, header = TRUE))
 
 magama_out$dataset <- rep(datasets, each = 9)
 
+head(magama_out)
+
 ## dirs
-plot_dir <- here("plots", "09_bulk_DE", "07_DE_plots")
+plot_dir <- here("plots", "13_MAGMA")
 if(!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
 
-magama_out |>
+magama_p_matrix <- magama_out |>
   select(VARIABLE, dataset, P) |>
-  pivot_wider(names_from = "dataset", values_from = "P")
+  pivot_wider(names_from = "dataset", values_from = "P") |>
+  column_to_rownames("VARIABLE") |>
+  as.matrix()
 
-# VARIABLE   scz2022 mdd2019edinburgh panic2019 sud2020op
-# <chr>        <dbl>            <dbl>     <dbl>     <dbl>
-# 1 Astrocyte  0.994             0.855      0.444   0.178  
-# 2 Endo       0.0735            0.0279     0.298   0.00228
-# 3 Excit.Thal 0.370             0.484      0.584   0.685  
-# 4 Inhib.Thal 0.548             0.0131     0.263   0.0969 
-# 5 LHb        0.553             0.905      0.422   0.681  
-# 6 MHb        0.180             0.859      0.340   0.649  
-# 7 Microglia  0.00500           0.105      0.319   0.423  
-# 8 OPC        0.988             0.747      0.676   0.612  
-# 9 Oligo      0.0340            0.348      0.950   0.753  
+#              scz2022 mdd2019edinburgh panic2019 sud2020op
+# Astrocyte  0.9935300         0.855180   0.44391 0.1784500
+# Endo       0.0735120         0.027865   0.29779 0.0022801
+# Excit.Thal 0.3703800         0.484070   0.58360 0.6848200
+# Inhib.Thal 0.5479500         0.013134   0.26306 0.0969340
+# LHb        0.5526100         0.904860   0.42161 0.6809000
+# MHb        0.1795000         0.858670   0.34027 0.6494700
+# Microglia  0.0050028         0.104720   0.31936 0.4231900
+# OPC        0.9881500         0.747370   0.67581 0.6124800
+# Oligo      0.0339570         0.347820   0.95005 0.7527400  
+
+mypal = c("white",grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "YlOrRd"))(50))
+
+magama_p_matrix_log <- -log10(magama_p_matrix)
+magama_p_matrix_anno <- round(magama_p_matrix_log,3)
+magama_p_matrix_anno[magama_p_matrix > 0.05] <- ""
+  
+pdf(here(plot_dir, "MAGMA_pval_heatmap.pdf"))
+Heatmap(magama_p_matrix_log,
+        col = mypal,
+        name = "-log10(p-val)",
+        rect_gp = grid::gpar(col = "black", lwd = 1),
+        cluster_rows = FALSE,
+        cluster_columns = FALSE,
+        cell_fun = function(j, i, x, y, width, height, fill) {
+          grid::grid.text(magama_p_matrix_anno[i, j], x, y, gp = grid::gpar(fontsize = 10))
+        })
+dev.off()
+
+
