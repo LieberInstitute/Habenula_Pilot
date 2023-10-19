deea795183fd92c0b695ec50d98dc5d70ab3ae13
lahuuki <lahuuki@gmail.com>
2023-10-19 13:38:45 -0400

    fix .genes.raw typo

--- code/13_MAGMA/10_MAGMA_gene_set_combo.sh
+++ code/13_MAGMA/10_MAGMA_gene_set_combo.sh
@@ -20,22 +20,22 @@ module load magma/1.10
 module list
 
 ## mdd2019edinburgh
-magma --gene-results ../../processed-data/13_MAGMA/GWAS/mdd2019edinburgh/PGC_UKB_depression_genome-wide\
+magma --gene-results ../../processed-data/13_MAGMA/GWAS/mdd2019edinburgh/PGC_UKB_depression_genome-wide.genes.raw\
 	--set-annot ../../processed-data/13_MAGMA/gene_sets/markerSets_broad_combo_ENSEMBL_FDR05.txt gene-col=Gene set-col=Set\
 	--out ../../processed-data/13_MAGMA/MAGMA_output/mdd2019edinburgh/mdd2019edinburgh_broad_combo
 
 ## panic2019
-magma --gene-results ../../processed-data/13_MAGMA/GWAS/panic2019/pgc-panic2019\
+magma --gene-results ../../processed-data/13_MAGMA/GWAS/panic2019/pgc-panic2019.genes.raw\
 	--set-annot ../../processed-data/13_MAGMA/gene_sets/markerSets_broad_combo_ENSEMBL_FDR05.txt gene-col=Gene set-col=Set\
 	--out ../../processed-data/13_MAGMA/MAGMA_output/panic2019/panic2019_broad_combo
 
 ## scz2022
-magma --gene-results ../../processed-data/13_MAGMA/GWAS/scz2022/PGC3_SCZ_wave3.european.autosome.public\
+magma --gene-results ../../processed-data/13_MAGMA/GWAS/scz2022/PGC3_SCZ_wave3.european.autosome.public.v3.genes.raw\
 	--set-annot ../../processed-data/13_MAGMA/gene_sets/markerSets_broad_combo_ENSEMBL_FDR05.txt gene-col=Gene set-col=Set\
 	--out ../../processed-data/13_MAGMA/MAGMA_output/scz2022/scz2022_broad_combo
 
 ## sud2020op
-magma --gene-results ../../processed-data/13_MAGMA/GWAS/sud2020op/opi.DEPvEXP_EUR.noAF\
+magma --gene-results ../../processed-data/13_MAGMA/GWAS/sud2020op/opi.DEPvEXP_EUR.noAF.genes.raw\
 	--set-annot ../../processed-data/13_MAGMA/gene_sets/markerSets_broad_combo_ENSEMBL_FDR05.txt gene-col=Gene set-col=Set\
 	--out ../../processed-data/13_MAGMA/MAGMA_output/sud2020op/sud2020op_broad_combo
 
