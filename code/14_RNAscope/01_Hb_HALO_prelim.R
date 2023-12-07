07d37605286905ffeb2f80ad048a6b8554779cdc
lahuuki <lahuuki@gmail.com>
2023-12-07 13:41:03 -0500

    Explore more plots for RNAscope

--- code/14_RNAscope/01_Hb_HALO_prelim.R
+++ code/14_RNAscope/01_Hb_HALO_prelim.R
@@ -61,8 +61,8 @@ copies_tab |> count(sum == 0)
 # 1 FALSE      14210
 # 2 TRUE        4411
 
-gg_copies <- copies_tab |> select(-sum) |> ggpairs()
-ggsave(gg_copies, filename = here(plot_dir, "gg_copies.png"), height = 9, width = 9)
+gg_copies <- copies_tab |> select(-sum) |> ggpairs(aes(alpha = 0.5))
+ggsave(gg_copies, filename = here(plot_dir, "gg_copies.png"), height = 12, width = 12)
 
 ## intensity
 halo_prelim |> 
@@ -155,12 +155,11 @@ walk(probes, ~{
   })
 
 ## facet 
-
 halo_copies_long <- halo_prelim |>
-  select(XMax, YMax, ends_with("Copies")) |>
-  pivot_longer(!c(XMax, YMax), names_to = "probe", values_to = "copies")
+  select(`Object Id`, XMin, XMax, YMin, YMax, ends_with("Copies")) |>
+  pivot_longer(!c(`Object Id`, XMin, XMax,YMin, YMax), names_to = "probe", values_to = "copies")
 
-hex_copies_facet <- ggplot(halo_copies_long) +
+hex_copies_median <- ggplot(halo_copies_long) +
   stat_summary_hex(aes(x = XMax, y = YMax, z = copies),
                    fun = median, bins = 100
   ) +
@@ -170,7 +169,118 @@ hex_copies_facet <- ggplot(halo_copies_long) +
   theme_bw() +
   facet_wrap(~probe)
 
-ggsave(hex_copies_facet, filename = here(plot_dir, paste0("hex_copies_median_facet.png")), height = 9, width = 9)
+ggsave(hex_copies_median, filename = here(plot_dir, paste0("hex_copies_median_facet.png")), height = 9, width = 9)
+
+
+hex_copies_mean <- ggplot(halo_copies_long) +
+  stat_summary_hex(aes(x = XMax, y = YMax, z = copies),
+                   fun = mean, bins = 100
+  ) +
+  # scale_fill_continuous(type = "viridis") + ## top value of 100 for visualization 
+  scale_fill_continuous(type = "viridis", limits = c(1,100), "Median Copies\n(max 100)") + ## top value of 100 for visualization
+  coord_equal() +
+  theme_bw() +
+  facet_wrap(~probe)
+
+ggsave(hex_copies_mean, filename = here(plot_dir, paste0("hex_copies_mean_facet.png")), height = 9, width = 9)
+
+
+hex_copies_any <- ggplot(halo_copies_long) +
+  stat_summary_hex(aes(x = XMax, y = YMax, z = copies > 0),
+                   fun = any, bins = 100
+  ) +
+  # scale_fill_continuous(type = "viridis") + ## top value of 100 for visualization 
+  # scale_fill_continuous(type = "viridis", limits = c(1,100), "Median Copies\n(max 100)") + ## top value of 100 for visualization
+  coord_equal() +
+  theme_bw() +
+  facet_wrap(~probe)
+
+
+ggsave(hex_copies_any, filename = here(plot_dir, paste0("hex_copies_any_facet.png")), height = 9, width = 9)
+
+#### distribution
+
+copies_density <- halo_copies_long |>
+  filter(copies != 0) |>
+  ggplot(aes(x = copies)) +
+  geom_histogram(binwidth = 1) +
+  theme_bw() +
+  xlim(0,50) +
+  facet_wrap(~probe, ncol =1)
+
+ggsave(copies_density, filename = here(plot_dir, "copies_denisty.png"))
+
+
+halo_prelim |> count(POUF1_present = `520 POU4F1 Copies` > 0)
+# POUF1_present     n
+# <lgl>         <int>
+# 1 FALSE         14555
+# 2 TRUE           4066
+
+
+cell_copies_facet <- halo_copies_long |>
+  filter(copies > 0) |>
+  ggplot() +
+  geom_rect(aes(
+    xmin = XMin, xmax = XMax,
+    ymin = YMin, ymax = YMax,
+    fill = copies
+  )) +
+  scale_fill_continuous(type = "viridis", limits = c(1,50), "Copies\n(max 50)")+
+  coord_equal()+
+  theme_bw() +
+  facet_wrap(~probe)
+
+ggsave(cell_copies_facet, filename = here(plot_dir, paste0("cell_copies_facet.png")), height = 9, width = 9)
+ggsave(cell_copies_facet, filename = here(plot_dir, paste0("cell_copies_facet.pdf")), height = 9, width = 9)
+
+
+## with POU4F1
+halo_POU4F1_long <- halo_prelim |>
+  select(`Object Id`, XMin, XMax, YMin, YMax, ends_with("Copies")) |>
+  rename(POU4F1 = `520 POU4F1 Copies`) |>
+  pivot_longer(!c(`Object Id`, XMin, XMax, YMin, YMax, POU4F1), names_to = "probe", values_to = "copies")
+
+halo_POU4F1_long |>
+  group_by(`Object Id`) |>
+  arrange(-copies) |>
+  slice(1) |>
+  ungroup() |>
+  count(POU4F1, probe)
+
+halo_POU4F1_long |>
+  filter(copies > 0) |>
+  group_by(probe) |>
+  count(POU4F1 > 0)
+
+# probe              `POU4F1 > 0`     n
+# <chr>              <lgl>        <int>
+# 1 570 SEMA3D Copies FALSE          7958
+# 2 570 SEMA3D Copies  TRUE          2814
+# 3 620 TLE2 Copies    FALSE         6358
+# 4 620 TLE2 Copies    TRUE          2275
+# 5 690 ONECUT2 Copies FALSE         2489
+# 6 690 ONECUT2 Copies TRUE           957
+
+
+cell_max_filter <- halo_POU4F1_long |>
+  group_by(`Object Id`) |>
+  arrange(-copies) |>
+  slice(1) |>
+  filter(copies >= 5) |>
+  ggplot() +
+  geom_rect(aes(
+    xmin = XMin, xmax = XMax,
+    ymin = YMin, ymax = YMax,
+    fill = probe
+  )) +
+  coord_equal()+
+  theme_bw() 
+
+ggsave(cell_max_filter, filename = here(plot_dir, paste0("cell_max_filter.png")), height = 9, width = 9)
+ggsave(cell_max_filter, filename = here(plot_dir, paste0("cell_max_filter.pdf")), height = 9, width = 9)
+
+
 
 #### Cell Type Annotations ####
 
@@ -199,8 +309,8 @@ ggsave(cell_slide, filename = here(plot_dir, "cell_slide.png"))
 
 ## facet
 halo_class_long <- halo_prelim |>
-  select(XMin, XMax,YMin, YMax, ends_with("Classification")) |>
-  pivot_longer(!c(XMin, XMax,YMin, YMax), names_to = "probe", values_to = "class") |>
+  select(`Object Id`, XMin, XMax,YMin, YMax, ends_with("Classification")) |>
+  pivot_longer(!c(`Object Id`, XMin, XMax,YMin, YMax), names_to = "probe", values_to = "class") |>
   mutate(class = as.factor(class))
 
 cell_class_facet <- ggplot(halo_class_long) +
@@ -217,6 +327,24 @@ cell_class_facet <- ggplot(halo_class_long) +
 
 ggsave(cell_class_facet, filename = here(plot_dir, paste0("cell_class_facet.png")), height = 9, width = 9)
 ggsave(cell_class_facet, filename = here(plot_dir, paste0("cell_class_facet.pdf")), height = 9, width = 9)
+
+## filter
+cell_class_facet_filter <- halo_class_long |>
+  filter(as.integer(class) > 2) |>
+  ggplot() +
+  geom_rect(aes(
+    xmin = XMin, xmax = XMax,
+    ymin = YMin, ymax = YMax,
+    fill = class
+  )) +
+  scale_fill_manual(values = c(`0` = "grey75", `1` = "#FECC5C", `2` = "#FD8D3C", `3` = "#F03B20", `4` = "#BD0026"), "class") +
+  # scale_fill_manual(values = c(`0` = "grey75", `1` = "#D7191C", `2` = "#FDAE61", `3` = "#ABDDA4", `4` = "#2B83BA")) +
+  coord_equal()+
+  theme_bw() +
+  facet_wrap(~probe)
+
+ggsave(cell_class_facet_filter, filename = here(plot_dir, paste0("cell_class_facet_filter.png")), height = 9, width = 9)
+ggsave(cell_class_facet_filter, filename = here(plot_dir, paste0("cell_class_facet_filter.pdf")), height = 9, width = 9)
 
 
 
