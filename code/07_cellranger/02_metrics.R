92380ead5bb6570728b5025dfcc781f35598c3c7
lcolladotor <lcolladotor@gmail.com>
2024-01-16 17:23:25 -0500

    Version control CellRanger metrics

---
+++ code/07_cellranger/02_metrics.R
@@ -0,0 +1,39 @@
+metrics_files <-
+    list.files(
+        here::here("processed-data", "07_cellranger"),
+        pattern = "metrics_summary.csv",
+        full.names = TRUE,
+        recursive = TRUE
+    )
+names(metrics_files) <-
+    list.files(here::here("processed-data", "07_cellranger"), pattern = "^Br")
+
+metrics <- do.call(rbind, lapply(metrics_files, read.csv))
+
+summary(as.numeric(gsub(",", "", metrics$Number.of.Reads)) / 1e6)
+#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 162.8   172.9   193.0   188.3   201.8   213.0
+
+summary(as.numeric(gsub(",", "", metrics$Mean.Reads.per.Cell)))
+#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 39476   45998   54641   88719  102062  230795
+
+summary(as.numeric(gsub(
+    ",", "", metrics$Median.UMI.Counts.per.Cell
+)))
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 7752   10856   14135   17674   22824   34469
+
+summary(as.numeric(gsub(",", "", metrics$Median.Genes.per.Cell)))
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 2836    3838    4592    5083    6316    7843
+
+write.csv(
+    cbind(data.frame(BrNum = rownames(metrics)), metrics),
+    file = here::here(
+        "processed-data",
+        "07_cellranger",
+        "STable_CellRanger_metrics.csv"
+    ),
+    row.names = FALSE
+)
