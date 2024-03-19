d8cbfd870855a48f5ead2d818908153e1f1acf52
Nick-Eagles <nick.eagles@libd.org>
2024-03-19 08:32:10 -0400

    Copy over tensorQTL preparation code from https://github.com/LieberInstitute/goesHyde_mdd_rnaseq/blob/master/eqtl/code/01_convert_rdata_to_tensorqtl_format.R
    Co-authored-by: Louise Huuki <lahuuki@gmail.com>

---
+++ code/17_eQTL/01_prep_inputs.R
@@ -0,0 +1,146 @@
+library("SummarizedExperiment")
+library("sessioninfo")
+library("tidyverse")
+library("VariantAnnotation")
+library("jaffelab")
+library("here")
+library("recount")
+library("data.table")
+
+source(here("eqtl", "code", "rse_to_bed.R"))
+
+## load
+load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
+load(here("exprs_cutoff", "rse_exon.Rdata"), verbose = TRUE)
+load(here("exprs_cutoff", "rse_jxn.Rdata"), verbose = TRUE)
+load(here("exprs_cutoff", "rse_tx.Rdata"), verbose = TRUE)
+
+## add logcounts
+message(Sys.time(), " - Calc Logcounts")
+assays(rse_gene)$logcounts <- log2(getRPKM(rse_gene, "Length")+1)
+assays(rse_exon)$logcounts <- log2(getRPKM(rse_exon, "Length")+1)
+assays(rse_jxn)$logcounts <- log2(getRPKM(rse_jxn, "Length")+1)
+assays(rse_tx)$logcounts <- log2(assays(rse_tx)$tpm+1)
+
+## Split gene data
+regions <- c(amyg = "Amygdala", sacc = "sACC")
+features <- c("gene", "exon", "jxn", "tx")
+# features <- c("gene","exon") ##to test
+names(features) <- features
+
+rse_gene_split <- map(regions, ~ rse_gene[, rse_gene$BrainRegion == .x])
+samples_split <- map(rse_gene_split, colnames)
+
+#### Covariate Data ####
+pcs <- map(features, function(f_name) map(regions, ~ get(load(here("eqtl", "data", "featuresPCs", paste0(f_name, "PCs", .x, ".rda"))))))
+corner(pcs$gene$amyg)
+
+covar_format <- function(data, rn) {
+    data <- as.data.frame(data)
+    rownames(data) <- rn
+    data <- t(data)
+    data <- as.data.frame(data) %>% rownames_to_column("id")
+    return(data)
+}
+
+message(Sys.time(), " - Format covariates")
+covars <- map2(
+    pcs, features,
+    function(pc, feat) {
+        pmap(list(rse = rse_gene_split, region = regions, pc = pc), function(rse, region, pc) {
+            message(paste(feat, region))
+            ## Phenodata
+            pd <- as.data.frame(colData(rse)[, c("PrimaryDx", "Sex", paste0("snpPC", 1:5))])
+            pd <- model.matrix(~ PrimaryDx + Sex + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5, data = pd)[, 2:9]
+            pd <- covar_format(pd, rse$genoSample)
+
+            ## PC data
+            pc <- covar_format(pc, rse$genoSample)
+            ## bind and save
+            covars <- rbind(pd, pc)
+            fn = here("eqtl", "data", "tensorQTL_input", "covariates_txt", paste0("covariates_", feat, "_", region, ".txt"))
+            message(fn)
+            write.table(covars,
+                file = fn,
+                sep = "\t", quote = FALSE, row.names = FALSE
+            )
+            return(covars)
+        })
+    }
+)
+corner(covars$gene$amyg)
+
+
+#### Expression Data ####
+
+## test TSS fix 8/24/23
+# bed <- rse_to_bed(rse_gene_split$amyg)
+# corner(bed)
+# tail(colnames(bed))
+# bed |> filter(ID == "ENSG00000000003.14") |> dplyr::select(ID, `#Chr`, `start`, `end`)
+# ID #Chr     start       end
+# 1 ENSG00000000003.14 chrX 100639991 100639992
+
+message(Sys.time(), " - logcounts to bed")
+expression_fn <- map(features, function(feat) map(regions, ~ here("eqtl", "data", "tensorQTL_input", "expression_bed", paste0(feat, "_", .x, ".bed.gz"))))
+
+expression_bed <- map2(list(rse_gene, rse_exon, rse_jxn, rse_tx), features, function(rse, feat) {
+    message(Sys.time(), " - ", feat)
+    rse_split <- map(regions, ~ rse[, rse$BrainRegion == .x])
+    expr_bed <- map(rse_split, rse_to_bed)
+    return(expr_bed)
+})
+
+## double check the output
+map_depth(expression_bed, 2, corner)
+
+message(Sys.time(), " - Write bed tables to .bed.gz files")
+walk2(expression_bed, expression_fn, function(expr, fn) {
+    walk2(expr, fn, ~ data.table::fwrite(.x, .y,
+        sep = "\t",
+        quote = FALSE, row.names = FALSE
+    ))
+})
+
+
+#### VCF ####
+# message(Sys.time(), " - Split VCF")
+# risk_vcf <- readVcf(here("eqtl", "data", "risk_snps", "LIBD_maf01_gwas_BPD.vcf.gz"))
+# risk_vcf
+# risk_vcf_split <- map(rse_gene_split, ~ risk_vcf[, .x$genoSample])
+# map(risk_vcf_split, dim)
+# 
+# vcf_fn <- map(regions, ~ here("eqtl", "data", "risk_snps", paste0("LIBD_maf01_gwas_BPD_", .x, ".vcf.gz")))
+# walk2(risk_vcf_split, vcf_fn, ~ writeVcf(.x, .y))
+# 
+# ## plink commands
+# map(vcf_fn, ~ paste("plink --make-bed --output-chr chrM --vcf", .x, "--out", gsub(".vcf.gz", "", .x)))
+# 
+
+## check
+
+# map2(bed, risk_vcf_split, ~ all(colnames(.x[, 5:ncol(.x)]) == colnames(.y)))
+# map2(bed, covars, ~ all(colnames(.x[, 5:ncol(.x)]) == colnames(.y[[1]][, 2:ncol(.y[[1]])])))
+
+
+#### prep interaction csv ####
+message(Sys.time(), " - Prep interaction data")
+walk2(rse_gene_split, regions, function(rse, region) {
+    cell_fractions <- colData(rse)[, c("Astro", "Endo", "Macro", "Micro", "Mural", "Oligo", "OPC", "Tcell", "Excit", "Inhib")]
+    cell_fractions <- as.data.frame(cell_fractions)
+    rownames(cell_fractions) <- rse$genoSample
+    write.csv(cell_fractions, file = here("eqtl", "data", "tensorQTL_input", "interaction", paste0("cell_fraction_", region, ".csv")))
+})
+
+## create shell commands ##
+# sgejobs::job_single("tensorqtl_risk_snps",
+#     create_shell = TRUE, queue = "bluejay", memory = "50G",
+#     command = "python tensorqtl_risk_snps.py"
+# )
+
+# sgejobs::job_single('01_convert_rdata_to_tensorqtl_format', create_shell = TRUE, memory = '50G', command = "Rscript 01_convert_rdata_to_tensorqtl_format.R")
+
+## Reproducibility information
+print("Reproducibility information:")
+options(width = 120)
+session_info()
