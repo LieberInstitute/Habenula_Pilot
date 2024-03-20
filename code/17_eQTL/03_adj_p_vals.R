63097d47cea35a091dddaf65be8638d0b9c3cd56
Nick-Eagles <nick.eagles@libd.org>
2024-03-20 10:53:36 -0400

    Grab code from https://github.com/LieberInstitute/goesHyde_mdd_rnaseq/blob/master/eqtl/code/04_filter_genomewide_nominal.R for adjusting p values
    Co-authored-by: Louise Huuki <lahuuki@gmail.com>

---
+++ code/17_eQTL/03_adj_p_vals.R
@@ -0,0 +1,67 @@
+library("tidyverse")
+library("jaffelab")
+library("miniparquet")
+library("sessioninfo")
+library("here")
+library("qs")
+
+## set up lists
+features <- c("gene", "exon", "jxn", "tx")
+names(features) <- features
+
+regions <- c(amyg = "Amygdala", sacc = "sACC")
+
+## loop throught datasets
+parquet_files <- map(
+    features,
+    function(f) {
+        map(
+            regions,
+            ~ list.files(
+                path = here("eqtl", "data", "tensorQTL_out", "genomewide_nominal"),
+                pattern = paste0(f, "_", .x),
+                full.names = TRUE
+            )
+        )
+    }
+)
+
+
+read_adj_filter <- function(parquet_files, cutoff = 0.05) {
+    eqtl_out <- do.call("rbind", map(parquet_files, parquet_read)) %>%
+        mutate(FDR = p.adjust(pval_nominal, "fdr")) 
+    message("n pairs: ", nrow(eqtl_out))
+    # filter
+    eqtl_out <- eqtl_out %>%
+        filter(FDR < cutoff)
+    message("n pairs FDR<", cutoff, ": ", nrow(eqtl_out))
+    return(eqtl_out)
+}
+
+# significant_snps <- c()
+eqtl_out_filtered <- map2(parquet_files, names(parquet_files), function(parq_feat, names_feat) {
+    map2(parq_feat, names(parq_feat), function(parq_region, names_region) {
+        message(paste("Reading:", names_feat, names_region, "- "), Sys.time())
+
+        eqtl_out_filtered <- read_adj_filter(parquet_files = parq_region)
+        
+        fn <- here("eqtl", "data", "tensorQTL_FDR05", "genomewide_nominal",
+          paste0(names_feat, "_", names_region, "_FDR05"))
+
+        write_csv(eqtl_out_filtered, file = paste0(fn, ".csv"))
+        
+        ## also save with qs to make loading faster later
+        qsave(eqtl_out_filtered, file = paste0(fn, ".qs"))
+    })
+})
+# significant_snps <- unique(significant_snps)
+# length(significant_snps)
+# cat(significant_snps, sep = "\n", file = here("eqtl", "data", "signif_snps", "significant_snps.txt"))
+
+# sgejobs::job_single('filter_genomewide_eqtl', create_shell = TRUE, memory = '100G', command = "Rscript filter_genomewide_eqtl.R")
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
