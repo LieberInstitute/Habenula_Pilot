382fa92e33d0e4731de514ad1de3be77edd3cd62
Nick-Eagles <nick.eagles@libd.org>
2024-03-20 13:32:40 -0400

    Use ordinary code instead of a function; remove the dataset-specific method for fixing genotypes

--- code/17_eQTL/02_run_tensorqtl.py
+++ code/17_eQTL/02_run_tensorqtl.py
@@ -13,82 +13,60 @@ print(f'Pandas {pd.__version__}')
 
 in_dir = Path(here("processed-data", "17_eQTL", "tensorQTL_input"))
 out_dir = Path(here("processed-data", "17_eQTL", "tensorQTL_output"))
-plink_prefix = str(
+plink_prefix_path = str(
     here("processed-data", '08_bulk_snpPC', "habenula_genotypes")
 )
+covariates_file = str(in_dir / "covariates.txt")
+expression_bed = str(in_dir / "logcounts.bed.gz")
+
 prefix = "habenula"
+add_chr = False
 
 ################################################################################
-#   Function definitions
+#   Load and ensure compatibility in input data
 ################################################################################
 
-def load_data(
-        plink_prefix_path, expression_bed, covariates_file, add_chr = False,
-        fix_geno_names = False
-    ):
-    # load phenotypes and covariates
-    print("Reading Expression file: " + expression_bed)
-    phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(expression_bed)
-    print("Phenotype dimensions:")
-    print(phenotype_df.shape)
+# load phenotypes and covariates
+print("Reading Expression file: " + expression_bed)
+phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(expression_bed)
+print("Phenotype dimensions:")
+print(phenotype_df.shape)
 
-    covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T
+covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T
 
-    print("." *20 )
-    # PLINK reader for genotypes
-    print("Reading Plink files: " + plink_prefix_path)
-    pr = genotypeio.PlinkReader(plink_prefix_path)
-    print("Loading Genotypes...", end='')
-    genotype_df = pr.load_genotypes()
-    variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
-    print("Genotype dimensions:", end='')
-    print(genotype_df.shape)
+print("." *20 )
+# PLINK reader for genotypes
+print("Reading Plink files: " + plink_prefix_path)
+pr = genotypeio.PlinkReader(plink_prefix_path)
+print("Loading Genotypes...", end='')
+genotype_df = pr.load_genotypes()
+variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
+print("Genotype dimensions:", end='')
+print(genotype_df.shape)
 
-    ## Fix genoSample names
-    if fix_geno_names:
-        print("Using fam to assign genoSample names")
-        fam = pd.read_table(
-            plink_prefix_path + '.fam',
-            delimiter = " ",
-            names = ["V" + str(i) for i in range(6)]
-        )
-        genoSamples = [
-            str(v1) + "_" + str(v0) for (v0,v1) in zip(fam.V1, fam.V0)
-        ]
-        genotype_df.columns = genoSamples
+## Fix chr names (maybe fix in plink?)
+if add_chr:
+    print("Adding 'chr' to genotype positions")
+    variant_df.chrom = [s.split(':')[0] for s in list(variant_df.index)]
 
-    ## Fix chr names (maybe fix in plink?)
-    if add_chr:
-        print("Adding 'chr' to genotype positions")
-        variant_df.chrom = [s.split(':')[0] for s in list(variant_df.index)]
+# Filter expression to chrom in snp data
+variant_chrom = set(variant_df.chrom)
+express_chrom = set(phenotype_pos_df.chr)
 
-    # Filter expression to chrom in snp data
-    variant_chrom = set(variant_df.chrom)
-    express_chrom = set(phenotype_pos_df.chr)
+if express_chrom - variant_chrom:
+    print("Excluding phenotypes from these chromosomes:")
+    print(express_chrom - variant_chrom)
 
-    if express_chrom - variant_chrom:
-        print("Excluding phenotypes from these chromosomes:")
-        print(express_chrom - variant_chrom)
+    chrom_filter = phenotype_pos_df.chr.isin(variant_chrom)
+    print("Phenotypes with chr in variants: " )
+    print(chrom_filter.value_counts())
 
-        chrom_filter = phenotype_pos_df.chr.isin(variant_chrom)
-        print("Phenotypes with chr in variants: " )
-        print(chrom_filter.value_counts())
-
-        phenotype_df = phenotype_df[chrom_filter]
-        phenotype_pos_df = phenotype_pos_df[chrom_filter]
-
-    return (genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
+    phenotype_df = phenotype_df[chrom_filter]
+    phenotype_pos_df = phenotype_pos_df[chrom_filter]
 
 ################################################################################
 #   Run tensorQTL
 ################################################################################
-
-covar = str(in_dir / "covariates.txt")
-expres = str(in_dir / "logcounts.bed.gz")
-
-genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df = load_data(
-    plink_prefix, expres, covar, add_chr = False, fix_geno_names = False
-)
 
 cis.map_nominal(
     genotype_df, variant_df, phenotype_df, phenotype_pos_df, prefix = prefix,
