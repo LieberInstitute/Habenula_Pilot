ba22f817de2b2b9c13482d78deab6160118e3361
Nick-Eagles <nick.eagles@libd.org>
2024-03-26 10:54:13 -0400

    Add initial buggy code to plot expression by genotype for DEA-significant genes associated with significant paired SNPs from eQTL analysis

--- code/17_eQTL/04_compare_DEA_GWAS.R
+++ code/17_eQTL/04_compare_DEA_GWAS.R
@@ -1,6 +1,9 @@
 library(here)
 library(tidyverse)
 library(readxl)
+library(SummarizedExperiment)
+library(snpStats)
+# library(bedr)
 
 eqtl_path = here('processed-data', '17_eQTL', 'tensorQTL_output', 'FDR05.csv')
 deg_path = here(
@@ -8,10 +11,18 @@ deg_path = here(
     'DEA_All-gene_qc-totAGene-qSVs-Hb-Thal.tsv'
 )
 gwas_path = here('processed-data', '17_eQTL', 'trubetskoy_gwas_supp_tab1.xls')
+rse_path = here(
+    'processed-data', 'rse_objects', 'rse_gene_Habenula_Pilot.rda'
+)
+plink_path_prefix = here(
+    "processed-data", '08_bulk_snpPC', "habenula_genotypes"
+)
+geno_path = here('processed-data', '08_bulk_snpPC', 'habenula_genotypes.vcf.gz')
 
 sig_cutoff = 0.05
 sig_cutoff_gwas = 5e-8
 
+#   Read in eQTL, DEA, and GWAS results as tibbles
 eqtl = read_csv(eqtl_path, show_col_types = FALSE)
 deg = read_tsv(deg_path, show_col_types = FALSE) |>
     filter(adj.P.Val < sig_cutoff)
@@ -35,3 +46,45 @@ message(
 
 table(deg$gencodeID %in% eqtl_gene$phenotype_id)
 deg$Symbol[deg$gencodeID %in% eqtl_gene$phenotype_id]
+
+overlap_variants = eqtl |>
+    filter(phenotype_id %in% deg$gencodeID) |>
+    pull(variant_id)
+
+rse_gene = get(load(rse_path))
+colnames(rse_gene) = rse_gene$BrNum
+
+#   Grab the expression for all genes significant in the DEA having significantly
+#   associated variants found in the eQTL analysis; convert to long format
+rse_small = rse_gene[
+    deg$gencodeID[deg$gencodeID %in% eqtl_gene$phenotype_id],
+]
+express = assays(rse_small)$logcounts |>
+    as.data.frame() |>
+    rownames_to_column("gene_id") |>
+    pivot_longer(cols = -gene_id, names_to = "sample_id", values_to = "logcount")
+
+#   Read in genotype data and join with expression data
+a = read.plink(paste0(plink_path_prefix, '.bed'))
+exp_df = a$genotypes[, overlap_variants] |>
+    as.data.frame() |>
+    rownames_to_column("sample_id") |>
+    pivot_longer(cols = -sample_id, names_to = "snp_id", values_to = "genotype") |>
+    mutate(
+        genotype = factor(as.integer(genotype)),
+        gene_id = eqtl$phenotype_id[match(snp_id, eqtl$variant_id)]
+    ) |>
+    left_join(express, by = c('sample_id', 'gene_id'))
+
+for (this_gene in unique(exp_df$gene_id)) {
+    p = exp_df |>
+        filter(gene_id == this_gene) |>
+        ggplot(mapping = aes(x = genotype, y = logcount)) +
+            geom_boxplot(outlier.shape = NA) +
+            geom_jitter() +
+            facet_wrap(~ snp_id)
+}
+
+# Started trying to read in VCF instead
+# a = read.vcf(geno_path)
+# b = a$vcf[match(overlap_variants, a$vcf$ID),]
