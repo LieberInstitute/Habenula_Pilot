fb19c2ec5324e8cc764e81d2c51e743ae86ebf48
Nick-Eagles <nick.eagles@libd.org>
2024-03-29 15:37:00 -0400

    Write a script showing that the genotypes read in from the plink bed are compatible with those read with VariantAnnotation::readVcf(), with a small adjustment and negation

---
+++ code/17_eQTL/06_compare_genotype_methods.R
@@ -0,0 +1,65 @@
+#   This script was run interactively to compare methods for reading in
+#   genotypes: from plink bed vs. VariantAnnotation::readVcf()
+
+library(here)
+library(tidyverse)
+library(SummarizedExperiment)
+library(snpStats)
+library(VariantAnnotation)
+
+plink_path = here(
+    "processed-data", '08_bulk_snpPC', "habenula_genotypes.bed"
+)
+paired_variants_path = here(
+    'processed-data', '17_eQTL', 'DEA_paired_variants.txt'
+)
+small_vcf_path = here(
+    'processed-data', '17_eQTL', 'paired_DEA_eQTL_SNPs.vcf'
+)
+
+paired_variants = readLines(paired_variants_path)
+
+#   Read in genotypes from the plink bed and tidy
+a = read.plink(plink_path)
+a = a$genotypes[, paired_variants] |>
+    as.data.frame() |>
+    rownames_to_column("sample_id") |>
+    pivot_longer(
+        cols = -sample_id, names_to = "snp_id", values_to = "genotype"
+    ) |>
+    mutate(genotype = as.integer(genotype)) |>
+    as_tibble()
+
+#   Read in genotypes from the subsetted VCF and tidy
+b = geno(readVcf(small_vcf_path))$GT |>
+    as.data.frame() |>
+    rownames_to_column("snp_id") |>
+    pivot_longer(
+        cols = -snp_id, names_to = "sample_id", values_to = "genotype"
+    ) |>
+    mutate(
+        genotype = case_when(
+            genotype == "0|0" ~ 0,
+            genotype == "1|1" ~ 2,
+            TRUE ~ 1
+        )
+    ) |>
+    as_tibble()
+
+#   Merge tibbles so genotype values are separate columns that can be compared
+#   side by side
+combined_df = left_join(
+    a |> dplyr::rename(genotype_a = genotype),
+    b |> dplyr::rename(genotype_b = genotype),
+    by = c('sample_id', 'snp_id')
+)
+
+#   None are equal as-is
+table(combined_df$genotype_a == combined_df$genotype_b)
+
+#   Are the numbers just in reverse order?
+combined_df_temp = combined_df |>
+    mutate(genotype_a = 3 - genotype_a)
+
+#   Ok, that was easy
+all(combined_df_temp$genotype_a == combined_df_temp$genotype_b)
