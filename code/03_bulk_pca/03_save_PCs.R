277a9479ed0d95c23504180293ee310aa817b899
Nick-Eagles <nick.eagles@libd.org>
2024-05-16 09:07:31 -0400

    Recompute gene PCs (really, just changing the number of PCs from 12 to 14) using the latest SNP PCs

--- code/03_bulk_pca/03_save_PCs.R
+++ code/03_bulk_pca/03_save_PCs.R
@@ -2,6 +2,7 @@ library("SummarizedExperiment")
 library("sessioninfo")
 library("here")
 library("sva")
+library("tidyverse")
 
 rse_path = here(
     'processed-data', 'rse_objects', 'rse_gene_Habenula_Pilot.rda'
@@ -10,8 +11,24 @@ out_path = here('processed-data', '03_bulk_pca', 'PCs.rds')
 covariates = c(
     "PrimaryDx", 'snpPC1', 'snpPC2', 'snpPC3', 'snpPC4', 'snpPC5'
 )
+snp_pcs_path = here(
+    'processed-data', '08_bulk_snpPC',
+    'Hb_gt_merged_R.9_MAF.05_ann_filt.snpPCs.tab'
+)
 
+#   Load RSE and overwrite SNP PCs with the most recent values computed from the
+#   properly filtered genotyping data
 rse = get(load(rse_path))
+colData(rse) = colData(rse) |>
+    as_tibble() |>
+    select(!matches('^snpPC')) |>
+    left_join(
+        read_tsv(snp_pcs_path, show_col_types = FALSE) |>
+            #   Fix the name of one donor
+            mutate(BrNum = ifelse(BrNum == "Br0983", "Br983", BrNum)),
+        by = 'BrNum'
+    ) |>
+    DataFrame()
 
 pd = as.data.frame(colData(rse)[, covariates])
 mod <- model.matrix(
