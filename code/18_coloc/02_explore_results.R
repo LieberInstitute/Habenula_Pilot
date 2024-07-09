9d879b692f148dbe2a396a1edc44448931bf3bcc
Nick-Eagles <nick.eagles@libd.org>
2024-07-09 14:42:48 -0400

    Compare coloc results with DEGs and eQTLs. Export info about overlaps with eQTLs

--- code/18_coloc/02_explore_results.R
+++ code/18_coloc/02_explore_results.R
@@ -1,10 +1,27 @@
 library(here)
 library(tidyverse)
 library(sessioninfo)
+library(SummarizedExperiment)
+library(MRutils)
 
 results_path = here("processed-data", "18_coloc", "coloc_results.rds")
 out_path = here("processed-data", "18_coloc", "filtered_results.csv")
+deg_path = here(
+    'processed-data', '10_DEA', '04_DEA',
+    'DEA_All-gene_qc-totAGene-qSVs-Hb-Thal.tsv'
+)
+eqtl_path = here(
+    'processed-data', '17_eQTL', 'tensorQTL_output', 'independent', 'FDR05.csv'
+)
+rse_path = here(
+    'processed-data', 'rse_objects', 'rse_gene_Habenula_Pilot.rda'
+)
+eqtl_overlap_path = here(
+    "processed-data", "18_coloc", "eqtl_overlap.csv"
+)
+
 h4_cutoff = 0.8
+deg_sig_cutoff = 0.1
 
 results = readRDS(results_path)
 
@@ -25,7 +42,7 @@ results_df = lapply(
     }
 )
 
-do.call(rbind, results_df) |>
+results_df = do.call(rbind, results_df) |>
     #   Only take genes with sufficient probability of shared causal variant
     filter(gene %in% all_genes[shared_causal_variant]) |>
     #   For each gene, take just the set of SNPs in the 95% credible set
@@ -35,7 +52,79 @@ do.call(rbind, results_df) |>
     filter(h4_cumsum <= 0.95) |>
     ungroup() |>
     #   Save just the gene, SNP, and shared-causal-variant probability
-    select(gene, snp, SNP.PP.H4) |>
-    write_csv(out_path)
+    select(gene, snp, SNP.PP.H4)
+
+write_csv(results_df, out_path)
+
+#   Load RSE just to get gene symbols
+rse_gene = get(load(rse_path))
+
+#   Print symbols of genes that colocalize
+coloc_genes = rowData(rse_gene)$Symbol[
+    match(unique(results_df$gene), rownames(rse_gene))
+]
+message(
+    'Names of colocalized genes: "',
+    paste(coloc_genes, collapse = '", "'),
+    '"'
+)
+
+#   Check if genes overlap with DEGs
+deg = read_tsv(deg_path, show_col_types = FALSE) |>
+    filter(adj.P.Val < deg_sig_cutoff)
+
+message(
+    sprintf(
+        "Any coloc genes differentially expressed (FDR < %s)?", deg_sig_cutoff
+    )
+)
+table(results_df$gene %in% deg$gencodeID)
+
+snp_map = tibble(snp_name = unique(eqtl$variant_id)) |>
+    mutate(
+        snp_rs_id = get_rsid_from_position(
+            chrom = sub('chr', '', str_split_i(snp_name, ':', 1)),
+            pos = str_split_i(snp_name, ':', 2),
+            ref = str_split_i(snp_name, ':', 3),
+            alt = str_split_i(snp_name, ':', 4),
+            assembly = "hg38"
+        )
+    )
+
+#   Check for overlaps with eQTLs
+eqtl = read_csv(eqtl_path, show_col_types = FALSE) |>
+    mutate(
+        is_coloc = paste(phenotype_id, variant_id, sep = '_') %in%
+            paste(results_df$gene, results_df$snp, sep = '_'),
+        
+    )
+
+message("Any coloc gene-SNP pairs also independent eQTLs?")
+table(eqtl$is_coloc)
+
+#   Write gene and SNP info for the colocalized gene-SNP pairs that overlap with
+#   independent eQTLs
+eqtl |>
+    filter(is_coloc) |>
+    mutate(
+        snp_rs_id = sapply(
+            variant_id,
+            function(x) {
+                get_rsid_from_position(
+                    chrom = str_split_i(x, ':', 1),
+                    pos = as.integer(str_split_i(x, ':', 2)),
+                    ref = str_split_i(x, ':', 3),
+                    alt = str_split_i(x, ':', 4),
+                    assembly = "hg38"
+                )
+            }
+        ),
+        gene_symbol = rowData(rse_gene)$Symbol[
+            match(phenotype_id, rownames(rse_gene))
+        ]
+    ) |>
+    dplyr::rename(gene_id = phenotype_id, snp_name = variant_id) |>
+    dplyr::select(gene_id, gene_symbol, snp_name, snp_rs_id) |>
+    write_csv(eqtl_overlap_path)
 
 session_info()
