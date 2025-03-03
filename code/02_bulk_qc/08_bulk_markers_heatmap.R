774595729fc10beb0f48ee7979362660fa5514b3
Louise Huuki <lahuuki@gmail.com>
2025-03-03 13:18:21 +0000

    Add litature genes, improve annotation colors, order by tot.Hb

--- code/02_bulk_qc/08_bulk_markers_heatmap.R
+++ code/02_bulk_qc/08_bulk_markers_heatmap.R
@@ -14,6 +14,8 @@ library("RColorBrewer")
 load(here("processed-data","rse_objects", "rse_gene_Habenula_Pilot.rda"),verbose = TRUE)
 #rse_gene
 
+rse_gene$PrimaryDx <- gsub("Schizo", "SCZD", rse_gene$PrimaryDx)
+
 # creating plot directory
 plot_dir <- here("plots", "02_bulk_qc", "08_bulk_markers_heatmap")
 if(!dir.exists(plot_dir)){
@@ -26,59 +28,12 @@ source(file = here("code", "99_paper_figs", "source_colors.R"))
 
 #### Check marker genes ####
 # list of marker genes
-official_markers = list(
-  "Oligo" = c("MOBP"),
-  "OPC" = c("PDGFRA"),
-  "Micro" = c("CSF1R"),
-  "Astro" = c("AQP4"),
-  "Endo" = c("ITIH5"),
-  "Thal" = c("LYPD6B", "ADARB2", "RORB"),
-  # "LHb.1" = c("LINC02653"), #  , ATP8B1
-  # "LHb.2" = c("AC073071.1"),
-  # "LHb.3" = c ("ENTHD1"),
-  # "LHb.4" = c("TLE2"),
-  # "LHb.5" = c("LINC01619"),
-  # "LHb.6" = c("TACR3"),
-  # "LHb.7" = c("AC008619.1"),
-  # "MHb.1" = c("EXOC1L"),
-  # "MHb.2" = c("CHAT"),
-  # "MHb.3" = c("BHLHE22"),
-  'Hb' = c("POU4F1", "GPR151", "TAC3"), # BARHL1
-  "MHb" = c("CHRNB4"),
-  "LHb" = c("HTR2C"),
-  'Neuron' = c('SYT1'),
-  'Exc_Neuron' = c('SLC17A6'),
-  'Inh_Neuron' = c('GAD1')
+lit_markers = tibble(
+    cellType.target = c("Hb", "Hb","MHb"),
+    Symbol = c("POU4F1", "GPR151", "TAC3"),
+    source = "Literature",
+    gene = rowData(rse_gene)$ensemblID[match(c("POU4F1", "GPR151", "TAC3"), rowData(rse_gene)$Symbol)]
 )
-
-marker_stats <- readxl::read_xlsx(here("plots", "99_paper_figs", "10c_snResolution_Top_Markers", "snResolution_top50MarkerGenes.xlsx"))
-marker_stats[[1]] <- NULL
-marker_stats |> count(cellType.target)
-
-official_markers_tb <- tibble(cellType.short = names(official_markers),
-                              cellType.target = names(official_markers),
-                              gene = official_markers) |>
-  unnest(gene) |>
-  mutate(cellType.target = case_when(cellType.target == "Astro" ~'Astrocyte',
-                                     cellType.target == "Micro" ~'Microglia',
-                                     TRUE ~ cellType.target))
-
-## Hb an Thal sub-type genes are datadriven (top mean ratio genes, glia is a mixed bag)
-marker_details <- marker_stats |>
-  right_join(official_markers_tb) |>
-  arrange(rank_ratio) |>
-  select(cellType.short, cellType.target, gene, rank_ratio, rank_marker) |>
-  mutate(final_cell_type = cellType.target %in% marker_stats$cellType.target,
-         anno = case_when(rank_ratio == 1 ~ "Data-Driven",
-                          !final_cell_type | cellType.target == "Microglia" ~ "Literature",
-                          TRUE ~ "Literature + Data-Supported"))
-
-marker_details |> print(n = 22)
-
-marker_stats |>
-  group_by(cellType.target) |>
-  arrange(rank_ratio) |>
-  slice(1:5)
 
 #### Mean Ratio Markers ####
 load(here("processed-data", "06_deconvolution", "run_Bisque", "marker_stats_top_25_genes.Rdata"), verbose = TRUE)
@@ -88,13 +43,18 @@ marker_stats |> count(cellType.target)
 markers_MR_top <- marker_stats |>
     filter(rank_ratio <= 5) |>
     select(cellType.target, gene, Symbol, rank_ratio) |>
+    mutate(source = "MeanRatio") |>
+    bind_rows(lit_markers) |>
     mutate(cellType.target = factor(cellType.target, levels = c("Astrocyte", "Endo","Microglia","Oligo","OPC",
-                                                                "Excit.Thal", "Inhib.Thal", "LHb","MHb")),
-           in_bulk = gene %in% rowData(rse_gene)$ensemblID,
-           ) |>
+                                                                       "Excit.Thal", "Inhib.Thal","Hb", "LHb","MHb")),
+                  in_bulk = gene %in% rowData(rse_gene)$ensemblID) |>
     arrange(cellType.target, rank_ratio)
 
+markers_MR_top |> print(n = 48)
+
 ## some genes are missing
+markers_MR_top |> filter(Symbol %in% lit_markers$Symbol)
+
 markers_MR_top |> filter(!in_bulk)
 # cellType.target gene            Symbol     rank_ratio in_bulk
 # <fct>           <chr>           <chr>           <int> <lgl>
@@ -114,51 +74,69 @@ rownames(bulk_logcounts) <- markers_MR_top$Symbol
 colnames(bulk_logcounts) <- rse_gene$BrNum
 
 dim(bulk_logcounts)
-# [1] 39 68
+# [1] 42 68
 
 marker_z_score <- scale(t(bulk_logcounts))
 
 #### prep complex heatmap annotations  ####
-# marker_method_colors <- c(`Data-Driven` = "#FFDA85",
-#                           `Literature + Data-Supported` = "#F7A5A1",
-#                           `Literature` = "#95E4EE")
-
 
 ## row sample annotations
 sample_anno <- as.data.frame(colData(rse_gene), row.names = NULL) |>
     mutate(sn = BrNum %in% c("Br1092", "Br1204", "Br1469", "Br1735", "Br5555", "Br5558", "Br5639")) |>
     select(tot.Hb, PrimaryDx, sn)
+
 rownames(sample_anno) <- rse_gene$BrNum
 
-row_ha <- rowAnnotation(df = sample_anno)
+## reorder matrix by tot.Hb
+sample_anno <- sample_anno |> arrange(tot.Hb)
+marker_z_score <- marker_z_score[rownames(sample_anno),]
+
+marker_z_score[1:5, 1:5]
+
+row_ha <- rowAnnotation(df = sample_anno,
+                        col = list(PrimaryDx = c(Control = "#2a9d8f", SCZD = "#f77f00"),
+                                   sn = c(`TRUE` = "black", `FALSE` = "white")))
+
 
 ## column marker gene annotations
 
-broad_cell_type_colors <- c(sn_colors,   "MHb" = c("#F33A6A"), "LHb" = c("#0000FF"))[levels(markers_MR_top$cellType.target)]
+broad_cell_type_colors <- c(sn_colors, "Hb" = "#702963",  "MHb" = "#F33A6A", "LHb" = "#0000FF")[levels(markers_MR_top$cellType.target)]
 
 column_ha <- HeatmapAnnotation(
     Marker_Gene = markers_MR_top$cellType.target,
-    col = list(Marker_Gene = broad_cell_type_colors)
+    source = markers_MR_top$source,
+    col = list(Marker_Gene = broad_cell_type_colors,
+               source = c(`MeanRatio` = "#FFDA85", `Literature` = "#95E4EE"))
 )
 
-heatmapped <- Heatmap(marker_z_score,
-                      name = "Z Score",
-                      col = circlize::colorRamp2(seq(-4, 4,  8/10),
-                                                 rev(RColorBrewer::brewer.pal(11, "RdBu"))),
-                      # cluster_rows = FALSE,
-                      cluster_columns = FALSE,
-                      right_annotation = row_ha,
-                      top_annotation = column_ha,
-                      column_split = markers_MR_top$cellType.target,
-                      # heatmap_legend_param = list(
-                      #   title = c("Z_Score"),
-                      #   border = "black"
-                      # )
-                      )
 
 # printing
 pdf(here(plot_dir, "Hb_bulk_heatmap_MRtop5.pdf"), width = 12, height = 10)
-  heatmapped
+print(Heatmap(marker_z_score,
+                      name = "Z Score",
+                      col = circlize::colorRamp2(seq(-4, 4,  8/10),
+                                                 rev(RColorBrewer::brewer.pal(11, "RdBu"))),
+                      cluster_rows = TRUE,
+                      cluster_columns = FALSE,
+                      right_annotation = row_ha,
+                      top_annotation = column_ha,
+                      column_split = markers_MR_top$cellType.target
+))
+
+dev.off()
+
+pdf(here(plot_dir, "Hb_bulk_heatmap_MRtop5_orderHb.pdf"), width = 12, height = 10)
+print(Heatmap(marker_z_score,
+                      name = "Z Score",
+                      col = circlize::colorRamp2(seq(-4, 4,  8/10),
+                                                 rev(RColorBrewer::brewer.pal(11, "RdBu"))),
+                      cluster_rows = FALSE,
+                      cluster_columns = FALSE,
+                      right_annotation = row_ha,
+                      top_annotation = column_ha,
+                      column_split = markers_MR_top$cellType.target
+))
+
 dev.off()
 
 
